90057008c31d04a13478b5a919de5ef95c7d0893
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2019-09-16 09:37:44 +0300

    make file include support vide moved to own file

---
+++ cc/video.c
@@ -0,0 +1,25 @@
+asm (".code16gcc");
+
+#include <video.h>
+
+void set_video(){
+	asm ("mov $0x0003, %ax\n"
+	     "int $0x10");
+}
+
+void clear_screen(){
+	int i;
+	for(i=0; i<VIDEO_BUF_LEN; i++) {
+		asm ("int $0x10" : : "a" (0x0e00 | ' '), "b" (0x0007));
+	}
+}
+
+void print(char *string, int x, int y)
+{
+	int i=0;
+	asm ("int $0x10" : : "a" (0x0200), "b" (0), "d" (y<<8|x));
+	while(string[i]!='\0') {
+		asm ("int $0x10" : : "a" (0x0e00 | string[i]), "b" (0x0007));
+		i++;
+	}
+}
---
+++ includes/video.h
@@ -0,0 +1,9 @@
+#ifndef ___VIDEO_H
+#define ___VIDEO_H 0
+
+const int VIDEO_BUF_LEN=25*80;
+void print(char *string, int x, int y);
+void clear_screen();
+void set_video();
+
+#endif
