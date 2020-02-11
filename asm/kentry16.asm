fabe9b99aba7c50cd14556c2624683e2761d043e
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2020-02-11 20:31:11 +0300

    slot type disk formatting and loading system

--- asm/kentry16.asm
+++ asm/kentry16.asm
@@ -3,15 +3,10 @@
 .global __kstart
 .global BOOT_DRIVE
 __kstart:
-  cmp    $0x9020, %ax
-  jne    __kstart_normal
-  mov    $0x0, %dl
-  jmp    $0x9000, $0x0
-
-__kstart_normal:
   pop %bx
 
   cli
+  call   enable_A20
   mov $0x100, %ax
   mov %ax, %ds
   mov %ax, %es
@@ -28,5 +23,48 @@ __kstart_normal:
 .loop:
   hlt
   jmp .loop
-.data
+
+
+enable_A20:
+  call   enable_A20.a20wait
+  mov    $0xad,%al
+  out    %al,$0x64
+
+  call   enable_A20.a20wait
+  mov    $0xd0,%al
+  out    %al,$0x64
+
+  call   enable_A20.a20wait2
+  in     $0x60,%al
+  push   %eax
+
+  call   enable_A20.a20wait
+  mov    $0xd1,%al
+  out    %al,$0x64
+
+  call   enable_A20.a20wait
+  pop    %eax
+  or     $0x2,%al
+  out    %al,$0x60
+
+  call   enable_A20.a20wait
+  mov    $0xae,%al
+  out    %al,$0x64
+
+  call   enable_A20.a20wait
+  ret
+
+  enable_A20.a20wait:
+  in     $0x64,%al
+  test   $0x2,%al
+  jne    enable_A20.a20wait
+  ret
+
+  enable_A20.a20wait2:
+  in     $0x64,%al
+  test   $0x1,%al
+  je     enable_A20.a20wait2
+  ret
+
+.bss
 BOOT_DRIVE: .byte 0x00
