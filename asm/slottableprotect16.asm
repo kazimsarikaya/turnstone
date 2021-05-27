c04fb74914dace793d95330de8c7e50f6880a8e4
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2021-05-27 12:44:52 +0300

    fix: pxe boot with undionly.kpxe
    if stage3 <=64k pxe boot can work
    
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- asm/slottableprotect16.asm
+++ asm/slottableprotect16.asm
@@ -1,43 +1,60 @@
 .code16
 .text
 .global __start
-.asciz "!PXE"
 
-// FIXME: read slot type 3 and calculate stage2 length.
 __start:
+jmp __realstart
+.asciz "!PXE"
+__realstart:
 cli
-mov    $slot_stage2.end, %eax
-mov    $slot_stage2.start, %ebx
+mov    %cs, %ax
+mov    %ax, %ds
+mov    $0x100, %ax
+mov    %ax, %es // destination stage will be at 0x1000
+mov    slot_stage2.end, %eax
+mov    slot_stage2.start, %ebx
+call    move_stage
+
+mov    %cs, %ax
+mov    %ax, %ds
+mov    $0x2000, %ax
+mov    %ax, %es // destination stage will be at 0x20000
+mov    slot_stage3.end, %eax
+mov    slot_stage3.start, %ebx
+call    move_stage
+
+jmp    start_stage2
+
+move_stage:
 sub    %ebx, %eax
 inc    %eax
-mov    $0x200, %ebx
-imul   %ebx, %eax // eax contains byte count of stage2
+mov    $0x200, %edx //sector byte count
+imul   %edx, %eax // eax contains byte count of stage2
+dec    %ebx // dec dx to relative this code segment
+mov    $0x20, %edx //sector size as segment
+imul   %ebx, %edx
 mov    %eax, %ecx // loop count
-push   %ds
-mov    $0x800, %ax // stage2 at 0x8000
+mov    %cs, %ax
+add    %dx, %ax // add offset as segment
 mov    %ax, %ds
-xor    %ax, %ax
-mov    %ax, %si // source index 0
-mov    $0x100, %ax // destination stage2 will be at 0x1000
-push   %es
-mov    %ax, %es
-xor    %ax, %ax
-mov    %ax, %di // destination index 0
+mov    $0x00, %esi // source index 0
+mov    $0x00, %edi // destination index 0
 
 cld
 rep    movsb
+ret
 
-pop    %es
-pop    %ds
-
+start_stage2:
 mov    $0x0, %ax // BOOT_DRIVE will be 0x0 for pxe boot
 push   %ax // send BOOT_DRIVE to stack
 xor    %eax, %eax
 jmp    $0x100, $0x0 // jmp to stage2
 
 
-
-.section .slottable
+// align slor_mbr to 0x110 with correct ld addressing
+slot_mbr_header:
+.balign 0x100, 0x00
+.quad 0,0
 slot_mbr:
 slot_mbr.type:
 .byte 1
@@ -65,3 +82,14 @@ slot_stage2.start:
 .byte 0, 0, 0, 0, 0, 0, 0, 0
 slot_stage2.end:
 .byte 0, 0, 0, 0, 0, 0, 0, 0
+slot_stage3:
+slot_stage3.type:
+.byte 4
+slot_stage3.unused:
+.byte 0, 0, 0, 0, 0, 0, 0
+slot_stage3.start:
+.byte 0, 0, 0, 0, 0, 0, 0, 0
+slot_stage3.end:
+.byte 0, 0, 0, 0, 0, 0, 0, 0
+slot_mbr_end:
+.balign 0x200, 0x00
