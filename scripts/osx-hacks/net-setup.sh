7bcb484d901384f9d7841e9ed4b4b5d273876f5d
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2021-05-29 04:21:27 +0300

    add: qemu on osx
    documentation and serveral scripts both pxe and hda boot
    
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

---
+++ scripts/osx-hacks/net-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo ifconfig bridge0 create
+sudo ifconfig bridge0 192.168.122.1 192.168.122.255
+sudo kextload  /Applications/Tunnelblick.app/Contents//Resources/tap-notarized.kext
+sudo port load dnsmasq
