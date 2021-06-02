c47b82d7936167ff2b9df45b7e56690c943835a4
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2021-06-02 10:35:27 +0300

    add: default va parsing
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- includes/types.h
+++ includes/types.h
@@ -101,4 +101,9 @@ typedef struct {
 int printf(const char* format, ...);
 #endif
 
+#define va_list     __builtin_va_list
+#define va_start(v, f) __builtin_va_start(v, f);
+#define va_end(v)       __builtin_va_end(v);
+#define va_arg(v, a)   __builtin_va_arg(v, a);
+
 #endif
