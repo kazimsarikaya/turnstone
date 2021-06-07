2215cb8767e28bc45cb432d0f572071b76635b81
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2021-06-07 12:24:08 +0300

    fix: printf signature at utils
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- utils/setup.h
+++ utils/setup.h
@@ -14,7 +14,7 @@
 #define GREENCOLOR "\033[1;32m"
 #define RESETCOLOR "\033[0m"
 
-int printf(const char* format, va_list args);
+int printf(const char* format, ...);
 
 size_t video_printf(char_t* fmt, va_list args) {
 	return printf(fmt, args);
