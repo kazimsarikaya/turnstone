144cdc048eb945e0d0edf75d56b43cc207ba0c56
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2021-06-29 20:57:24 +0300

    add: qemu disk as usb mass storage
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

---
+++ scripts/osx-hacks/qemu-usb.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+CURRENTDIR=`dirname $0`
+BASEDIR="${CURRENTDIR}/../../"
+OUTPUTDIR="${BASEDIR}/output"
+
+sudo qemu-system-x86_64 \
+  -M q35 -m 1g -smp cpus=2 -name osdev-usb-boot \
+  -drive if=none,id=stick,format=raw,file=${OUTPUTDIR}/qemu-hda \
+  -device nec-usb-xhci,id=xhci \
+  -device usb-storage,bus=xhci.0,drive=stick \
+  -net nic,model=virtio,macaddr=54:54:00:55:55:55 \
+  -net tap,script=${CURRENTDIR}/tap-up.sh,downscript=${CURRENTDIR}/tap-down.sh  \
+  -monitor stdio \
+  -serial file:${BASEDIR}/tmp/qemu-video.log
