66d85ddfc86f16bdaa43d6174f683fedb3c3859f
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2021-07-19 12:08:15 +0300

    add: error reporting for disk
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- scripts/osx-hacks/qemu-hda.sh
+++ scripts/osx-hacks/qemu-hda.sh
@@ -5,7 +5,7 @@ OUTPUTDIR="${BASEDIR}/output"
 
 sudo qemu-system-x86_64 \
   -M q35 -m 1g -smp cpus=2 -name osdev-hda-boot \
-  -drive index=0,media=disk,format=raw,file=${OUTPUTDIR}/qemu-hda \
+  -drive index=0,media=disk,format=raw,file=${OUTPUTDIR}/qemu-hda,werror=report,rerror=report \
   -net nic,model=virtio,macaddr=54:54:00:55:55:55 \
   -net tap,script=${CURRENTDIR}/tap-up.sh,downscript=${CURRENTDIR}/tap-down.sh  \
   -monitor stdio \
