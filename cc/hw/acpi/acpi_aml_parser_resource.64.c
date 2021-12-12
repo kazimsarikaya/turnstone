56d14f0de7274200c5e216e16a9d3424e265a2d7
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2021-12-12 13:17:40 +0300

    add: acpi aml resource parsing start
    for crs etc
    
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

---
+++ cc/hw/acpi/acpi_aml_parser_resource.64.c
@@ -0,0 +1,7 @@
+/**
+ * @file acpi_aml_parser_resource.64.c
+ * @brief acpi parser methods for device resources
+ */
+
+ #include <acpi/aml_internal.h>
+ #include <video.h>
--- includes/acpi/aml.h
+++ includes/acpi/aml.h
@@ -138,6 +138,208 @@ typedef struct _acpi_aml_object_t {
 	};
 }acpi_aml_object_t;
 
+typedef enum {
+	ACPI_AML_RESOURCE_SMALLITEM=0,
+	ACPI_AML_RESOURCE_LARGEITEM,
+}acpi_aml_resource_type_t;
+
+typedef enum {
+	ACPI_AML_RESOURCE_SMALLITEM_IRQ=4,
+	ACPI_AML_RESOURCE_SMALLITEM_DMA=5,
+	ACPI_AML_RESOURCE_SMALLITEM_START_DEPFUNC=6,
+	ACPI_AML_RESOURCE_SMALLITEM_END_DEPFUNC=7,
+	ACPI_AML_RESOURCE_SMALLITEM_IO=8,
+	ACPI_AML_RESOURCE_SMALLITEM_FIXEDIO=9,
+	ACPI_AML_RESOURCE_SMALLITEM_FIXEDDMA=10,
+	ACPI_AML_RESOURCE_SMALLITEM_VENDOR=14,
+	ACPI_AML_RESOURCE_SMALLITEM_ENDTAG=15,
+}acpi_aml_resource_smallitem_name_t;
+
+typedef enum {
+	ACPI_AML_RESOURCE_LARGEITEM_24BIT_MEMORY_RANGE=1,
+	ACPI_AML_RESOURCE_LARGEITEM_GENERIC_REGISTER=2,
+	ACPI_AML_RESOURCE_LARGEITEM_VENDOR=4,
+	ACPI_AML_RESOURCE_LARGEITEM_32BIT_MEMORY_RANGE=5,
+	ACPI_AML_RESOURCE_LARGEITEM_32BIT_FIXEDMEMORY_RANGE=6,
+	ACPI_AML_RESOURCE_LARGEITEM_DWORDADDRESS_SPACE=7,
+	ACPI_AML_RESOURCE_LARGEITEM_WORD_ADDRESS_SPACE=8,
+	ACPI_AML_RESOURCE_LARGEITEM_EXTENDED_INTERRUPT=9,
+	ACPI_AML_RESOURCE_LARGEITEM_QWORD_ADDRESS_SPACE=10,
+	ACPI_AML_RESOURCE_LARGEITEM_EXTENDED_ADDRESS_SPACE=11,
+	ACPI_AML_RESOURCE_LARGEITEM_GPIO_CONNECTION=12,
+	ACPI_AML_RESOURCE_LARGEITEM_PIN_FUNCTION=13,
+	ACPI_AML_RESOURCE_LARGEITEM_GENERIC_SERIAL_BUS_CONNECTION=14,
+	ACPI_AML_RESOURCE_LARGEITEM_PIN_CONFIGURATION=15,
+	ACPI_AML_RESOURCE_LARGEITEM_PIN_GROUP=16,
+	ACPI_AML_RESOURCE_LARGEITEM_PIN_GROUP_FUNCTION=17,
+	ACPI_AML_RESOURCE_LARGEITEM_PIN_GROUP_CONFIGURATION=18,
+}acpi_aml_resource_largeitem_name_t;
+
+typedef union {
+	struct {
+		uint8_t unused : 7;
+		acpi_aml_resource_type_t type : 1;
+	}__attribute__((packed)) type;
+
+	struct {
+		uint8_t length : 3;
+		acpi_aml_resource_smallitem_name_t name : 4;
+		acpi_aml_resource_type_t type : 1;
+
+		union {
+			struct {
+				uint16_t irq_masks : 16;
+				uint8_t mode : 1;
+				uint8_t ignored : 2;
+				uint8_t polarity : 1;
+				uint8_t sharing : 1;
+				uint8_t wake_capability : 1;
+				uint8_t reserved : 2;
+			}__attribute__((packed)) irq;
+
+			struct {
+				uint8_t channels : 8;
+				uint8_t size : 2;
+				uint8_t bus_master_status : 1;
+				uint8_t ignored : 2;
+				uint8_t speed_type : 2;
+				uint8_t zero : 1;
+			}__attribute__((packed)) dma;
+
+			struct {
+				uint8_t compability : 2;
+				uint8_t performance : 2;
+				uint8_t reserved : 4;
+			}__attribute__((packed)) start_depfunc;
+
+			struct {
+				uint8_t decode : 1;
+				uint8_t unused : 7;
+				uint16_t min : 16;
+				uint16_t max : 16;
+				uint8_t align : 8;
+				uint8_t length : 8;
+			}__attribute__((packed)) io;
+
+			struct {
+				uint16_t base : 10;
+				uint8_t unused : 6;
+				uint16_t length : 16;
+			}__attribute__((packed)) fixedio;
+
+			struct {
+				uint16_t request_line : 16;
+				uint16_t channel : 16;
+				uint8_t size : 8;
+			}__attribute__((packed)) fixeddma;
+
+			struct {
+				uint8_t checksum : 8;
+			}__attribute__((packed)) end_tag;
+
+		}__attribute__((packed));
+
+	}__attribute__((packed)) smallitem;
+
+	struct {
+		acpi_aml_resource_largeitem_name_t name : 7;
+		acpi_aml_resource_type_t type : 1;
+		uint16_t length : 16;
+
+		union {
+			struct {
+				uint8_t readwrite : 1;
+				uint8_t ignored : 7;
+				uint16_t min : 16;
+				uint16_t max : 16;
+				uint16_t align : 16;
+				uint16_t length : 16;
+			}__attribute__((packed)) memory_range_24bit;
+
+			struct {
+
+			}__attribute__((packed)) vendor;
+
+			struct {
+
+			}__attribute__((packed)) memory_range_32bit;
+
+			struct {
+
+			}__attribute__((packed)) memory_range_32bit_fixed;
+
+			struct {
+
+			}__attribute__((packed)) word_address_space;
+
+			struct {
+
+			}__attribute__((packed)) dword_address_space;
+
+			struct {
+
+			}__attribute__((packed)) qword_address_space;
+
+			struct {
+
+			}__attribute__((packed)) extended_address_space;
+
+			struct {
+
+			}__attribute__((packed)) extended_interrupt;
+
+			struct {
+
+			}__attribute__((packed)) generic_register;
+
+			struct {
+
+			}__attribute__((packed)) gpio_descriptor;
+
+			struct {
+
+			}__attribute__((packed)) generic_serial_bus;
+
+			struct {
+
+			}__attribute__((packed)) pin_function;
+
+			struct {
+
+			}__attribute__((packed)) pin_configuration;
+
+			struct {
+
+			}__attribute__((packed)) pin_group;
+
+			struct {
+				uint8_t revision_id : 8;
+				uint16_t flags : 16;
+				uint16_t function : 16;
+				uint8_t source_index : 8;
+				uint16_t source_name_offset : 16;
+				uint16_t source_label_offset : 16;
+				uint16_t vendor_data_offset : 16;
+				uint16_t vendor_data_length : 16;
+			}__attribute__((packed)) pin_group_function;
+
+			struct {
+				uint8_t revision_id : 8;
+				uint16_t flags : 16;
+				uint8_t type : 8;
+				uint32_t value : 32;
+				uint8_t source_index : 8;
+				uint16_t source_name_offset : 16;
+				uint16_t source_label_offset : 16;
+				uint16_t vendor_data_length : 16;
+			}__attribute__((packed)) pin_group_configuration;
+
+		}__attribute__((packed));
+
+	}__attribute__((packed)) largeitem;
+
+}__attribute__((packed)) acpi_aml_resource_t;
+
 acpi_aml_parser_context_t* acpi_aml_parser_context_create_with_heap(memory_heap_t*, uint8_t, uint8_t*, int64_t);
 #define acpi_aml_parser_context_create(rev, aml, len) acpi_aml_parser_context_create_with_heap(NULL, rev, aml, len)
 void acpi_aml_parser_context_destroy(acpi_aml_parser_context_t*);
