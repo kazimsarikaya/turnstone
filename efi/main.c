212734db8cb487b70b449008ca1778a83854e1da
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2021-12-27 22:06:24 +0300

    update: efi also allocates 1mib-2mib
    1mib-2mib area is required for kernel. satisfing always allocating
    that area
    
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- efi/main.c
+++ efi/main.c
@@ -127,125 +127,132 @@ int64_t efi_main(efi_handle_t image, efi_system_table_t* system_table) {
 
 				} // end of iter over all disks
 
-				if(sys_disk_idx != -1) {
-					printf("openning sys disk %li\n", sys_disk_idx);
+				uint64_t frm_start_1mib = 1 << 20;
 
-					disk_t* sys_disk = efi_disk_impl_open(blk_devs[sys_disk_idx].bio);
+				if(BS->allocate_pages(EFI_ALLOCATE_ADDRESS, EFI_LOADER_DATA, 0x100, &frm_start_1mib) == 0) {
 
-					if(sys_disk) {
-						printf("openning as gpt disk\n");
+					if(sys_disk_idx != -1) {
+						printf("openning sys disk %li\n", sys_disk_idx);
 
-						sys_disk = gpt_get_or_create_gpt_disk(sys_disk);
+						disk_t* sys_disk = efi_disk_impl_open(blk_devs[sys_disk_idx].bio);
 
-						printf("gpt disk getted\n");
+						if(sys_disk) {
+							printf("openning as gpt disk\n");
 
-						disk_partition_context_t* part_ctx = sys_disk->get_partition(sys_disk, 1);
+							sys_disk = gpt_get_or_create_gpt_disk(sys_disk);
 
-						printf("kernel start lba %x end lba %x\n", part_ctx->start_lba, part_ctx->end_lba);
+							printf("gpt disk getted\n");
 
-						uint8_t* kernel_data;
-						int64_t kernel_size = (part_ctx->end_lba - part_ctx->start_lba  + 1) * blk_devs[sys_disk_idx].bio->media->block_size;
+							disk_partition_context_t* part_ctx = sys_disk->get_partition(sys_disk, 1);
 
-						printf("kernel size %li\n", kernel_size);
+							printf("kernel start lba %x end lba %x\n", part_ctx->start_lba, part_ctx->end_lba);
 
-						if(sys_disk->read(sys_disk, part_ctx->start_lba, part_ctx->end_lba - part_ctx->start_lba  + 1, &kernel_data) == 0) {
-							printf("kernel loaded at 0x%p\n", kernel_data);
+							uint8_t* kernel_data;
+							int64_t kernel_size = (part_ctx->end_lba - part_ctx->start_lba  + 1) * blk_devs[sys_disk_idx].bio->media->block_size;
 
-							int64_t kernel_page_count = kernel_size / 4096 + 0x120; // adding extra pages for stack and heap
-							if(kernel_size % 4096) {
-								kernel_page_count++;
-							}
+							printf("kernel size %li\n", kernel_size);
 
-							int64_t new_kernel_2m_factor = 0;
-							new_kernel_2m_factor = kernel_page_count / 512;
-							if(kernel_page_count % 512) {
-								new_kernel_2m_factor++;
-							}
-							kernel_page_count = new_kernel_2m_factor * 512;
+							if(sys_disk->read(sys_disk, part_ctx->start_lba, part_ctx->end_lba - part_ctx->start_lba  + 1, &kernel_data) == 0) {
+								printf("kernel loaded at 0x%p\n", kernel_data);
 
-							printf("new kernel page count 0x%lx\n", kernel_page_count);
+								int64_t kernel_page_count = kernel_size / 4096 + 0x120;         // adding extra pages for stack and heap
+								if(kernel_size % 4096) {
+									kernel_page_count++;
+								}
 
-							uint64_t new_kernel_address = 2 << 20;
+								int64_t new_kernel_2m_factor = 0;
+								new_kernel_2m_factor = kernel_page_count / 512;
+								if(kernel_page_count % 512) {
+									new_kernel_2m_factor++;
+								}
+								kernel_page_count = new_kernel_2m_factor * 512;
 
-							if(BS->allocate_pages(EFI_ALLOCATE_ADDRESS, EFI_LOADER_DATA, kernel_page_count, &new_kernel_address) == 0) {
-								printf("alloc pages for new kernel succed at 0x%lx\n", new_kernel_address);
+								printf("new kernel page count 0x%lx\n", kernel_page_count);
 
-								if(linker_memcopy_program_and_relink((size_t)kernel_data, new_kernel_address, ((size_t)kernel_data) + 0x100 - 1) == 0) {
-									printf("moving kernel at 0x%lx succed\n", new_kernel_address);
-									memory_free(kernel_data);
+								uint64_t new_kernel_address = 2 << 20;
+
+								if(BS->allocate_pages(EFI_ALLOCATE_ADDRESS, EFI_LOADER_DATA, kernel_page_count, &new_kernel_address) == 0) {
+									printf("alloc pages for new kernel succed at 0x%lx\n", new_kernel_address);
+
+									if(linker_memcopy_program_and_relink((size_t)kernel_data, new_kernel_address, ((size_t)kernel_data) + 0x100 - 1) == 0) {
+										printf("moving kernel at 0x%lx succed\n", new_kernel_address);
+										memory_free(kernel_data);
 
 
-									printf("conf table count %i\n", system_table->configuration_table_entry_count);
-									efi_guid_t acpi_table_v2_guid = EFI_ACPI_20_TABLE_GUID;
-									efi_guid_t acpi_table_v1_guid = EFI_ACPI_TABLE_GUID;
+										printf("conf table count %i\n", system_table->configuration_table_entry_count);
+										efi_guid_t acpi_table_v2_guid = EFI_ACPI_20_TABLE_GUID;
+										efi_guid_t acpi_table_v1_guid = EFI_ACPI_TABLE_GUID;
 
-									void* acpi_rsdp = NULL;
-									void* acpi_xrsdp = NULL;
+										void* acpi_rsdp = NULL;
+										void* acpi_xrsdp = NULL;
 
-									for (uint64_t i = 0; i <  system_table->configuration_table_entry_count; i++ ) {
-										if(efi_guid_equal(acpi_table_v2_guid, system_table->configuration_table[i].vendor_guid) == 0) {
-											acpi_xrsdp = system_table->configuration_table[i].vendor_table;
-										} else if(efi_guid_equal(acpi_table_v1_guid, system_table->configuration_table[i].vendor_guid) == 0) {
-											acpi_rsdp = system_table->configuration_table[i].vendor_table;
+										for (uint64_t i = 0; i <  system_table->configuration_table_entry_count; i++ ) {
+											if(efi_guid_equal(acpi_table_v2_guid, system_table->configuration_table[i].vendor_guid) == 0) {
+												acpi_xrsdp = system_table->configuration_table[i].vendor_table;
+											} else if(efi_guid_equal(acpi_table_v1_guid, system_table->configuration_table[i].vendor_guid) == 0) {
+												acpi_rsdp = system_table->configuration_table[i].vendor_table;
+											}
 										}
-									}
 
-									uint8_t* mmap = NULL;
-									uint64_t map_size, map_key, descriptor_size;
-									uint32_t descriptor_version;
+										uint8_t* mmap = NULL;
+										uint64_t map_size, map_key, descriptor_size;
+										uint32_t descriptor_version;
 
-									BS->get_memory_map(&map_size, (efi_memory_descriptor_t*)mmap, &map_key, &descriptor_size, &descriptor_version);
-									printf("mmap size %li desc size %li ver %li\n", map_size, descriptor_size, descriptor_version);
+										BS->get_memory_map(&map_size, (efi_memory_descriptor_t*)mmap, &map_key, &descriptor_size, &descriptor_version);
+										printf("mmap size %li desc size %li ver %li\n", map_size, descriptor_size, descriptor_version);
 
-									mmap = memory_malloc(map_size);
+										mmap = memory_malloc(map_size);
 
-									if(BS->get_memory_map(&map_size, (efi_memory_descriptor_t*)mmap, &map_key, &descriptor_size, &descriptor_version) == EFI_SUCCESS) {
-										system_info_t* sysinfo = memory_malloc(sizeof(system_info_t));
-										sysinfo->boot_type = SYSTEM_INFO_BOOT_TYPE_DISK;
-										sysinfo->mmap_data = mmap;
-										sysinfo->mmap_size = map_size;
-										sysinfo->mmap_descriptor_size = descriptor_size;
-										sysinfo->mmap_descriptor_version = descriptor_version;
-										sysinfo->frame_buffer = vfb;
-										sysinfo->acpi_version = acpi_xrsdp != NULL?2:1;
-										sysinfo->acpi_table = acpi_xrsdp != NULL?acpi_xrsdp:acpi_rsdp;
-										sysinfo->kernel_start = new_kernel_address;
-										sysinfo->kernel_4k_frame_count = kernel_page_count;
+										if(BS->get_memory_map(&map_size, (efi_memory_descriptor_t*)mmap, &map_key, &descriptor_size, &descriptor_version) == EFI_SUCCESS) {
+											system_info_t* sysinfo = memory_malloc(sizeof(system_info_t));
+											sysinfo->boot_type = SYSTEM_INFO_BOOT_TYPE_DISK;
+											sysinfo->mmap_data = mmap;
+											sysinfo->mmap_size = map_size;
+											sysinfo->mmap_descriptor_size = descriptor_size;
+											sysinfo->mmap_descriptor_version = descriptor_version;
+											sysinfo->frame_buffer = vfb;
+											sysinfo->acpi_version = acpi_xrsdp != NULL?2:1;
+											sysinfo->acpi_table = acpi_xrsdp != NULL?acpi_xrsdp:acpi_rsdp;
+											sysinfo->kernel_start = new_kernel_address;
+											sysinfo->kernel_4k_frame_count = kernel_page_count;
 
-										printf("calling kernel with sysinfo @ 0x%p\n", sysinfo);
+											printf("calling kernel with sysinfo @ 0x%p\n", sysinfo);
 
-										BS->exit_boot_services(image, map_key);
+											BS->exit_boot_services(image, map_key);
 
-										kernel_start_t ks = (kernel_start_t)new_kernel_address;
-
-
-										ks(sysinfo);
+											kernel_start_t ks = (kernel_start_t)new_kernel_address;
 
 
-									} else {
-										printf("cannot fill memory map\n");
-									}
+											ks(sysinfo);
 
-								} // endof linker
+
+										} else {
+											printf("cannot fill memory map\n");
+										}
+
+									}         // endof linker
+
+								} else {
+									printf("cannot alloc pages for new kernel\n");
+									memory_free(kernel_data);
+								}
 
 							} else {
-								printf("cannot alloc pages for new kernel\n");
+								printf("kernel load failed\n");
 								memory_free(kernel_data);
 							}
 
+
+
+
+							memory_free(part_ctx);
 						} else {
-							printf("kernel load failed\n");
-							memory_free(kernel_data);
+							printf("sys disk open failed\n");
 						}
 
-
-
-
-						memory_free(part_ctx);
-					} else {
-						printf("sys disk open failed\n");
 					}
-
+				} else {
+					printf("cannot allocate frame for kernel usage at 1mib to 2mib\n");
 				}
 
 			} // end of malloc success
@@ -260,7 +267,7 @@ int64_t efi_main(efi_handle_t image, efi_system_table_t* system_table) {
 		printf("memory pool creation failed. err code 0x%x\n", res);
 	}
 
-	printf("efi app finished, infinite loop started.\n");
+	printf("efi app could not have finished correctly, infinite loop started.\n");
 
 	while(1);
 
