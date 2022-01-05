301ca3cfdd9fd278d16a5808d4666d5dab5c69e4
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2022-01-05 12:35:03 +0300

    fix: PRINTLOG macro
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- tests/setup.h
+++ tests/setup.h
@@ -15,17 +15,21 @@
 #define RESETCOLOR "\033[0m"
 
 int printf(const char* format, ...);
+int vprintf ( const char* format, va_list arg );
 
-size_t video_printf(char_t* fmt, va_list args) {
-	return printf(fmt, args);
+size_t video_printf(char_t* fmt, ...) {
+	va_list args;
+	va_start(args, fmt);
+	int res = vprintf(fmt, args);
+	va_end(args);
+
+	return res;
 }
 
 uint8_t mem_area[RAMSIZE] = {0};
 uint64_t __kheap_bottom = 0;
 void* SYSTEM_INFO = NULL;
 void* KERNEL_FRAME_ALLOCATOR = NULL;
-
-#define PRINTLOG(module, type, msg, ...)  printf("%s:%i: " module " : " type " " msg "\n", __FILE__, __LINE__, __VA_ARGS__)
 
 void print_success(const char* msg){
 	printf("%s%s%s%s", GREENCOLOR, msg, RESETCOLOR, "\r\n");
@@ -34,6 +38,10 @@ void print_success(const char* msg){
 void print_error(const char* msg){
 	printf("%s%s%s%s", REDCOLOR, msg, RESETCOLOR, "\r\n");
 }
+
+#define PRINTLOG(M, L, msg, ...)  if(LOG_NEED_LOG(M, L)) { \
+		if(LOG_LOCATION) { video_printf("%s:%i:%s:%s: " msg "\n", __FILE__, __LINE__, logging_module_names[M], logging_level_names[L], __VA_ARGS__); } \
+		else {video_printf("%s:%s: " msg "\n", logging_module_names[M], logging_level_names[L], __VA_ARGS__); } }
 
 typedef void frame_t;
 typedef int8_t memory_paging_page_type_t;
@@ -46,10 +54,11 @@ int8_t memory_paging_add_va_for_frame_ext(memory_page_table_t* p4, uint64_t va_s
 	UNUSED(type);
 	return 0;
 }
+memory_heap_t* heap = NULL;
 
 void setup_ram() {
-	memory_heap_t* heap = memory_create_heap_simple((size_t)&mem_area[0], (size_t)&mem_area[RAMSIZE]);
-	printf("%p\n", heap);
+	heap = memory_create_heap_simple((size_t)&mem_area[0], (size_t)&mem_area[RAMSIZE]);
+	//printf("%p\n", heap);
 	memory_set_default_heap(heap);
 }
 
