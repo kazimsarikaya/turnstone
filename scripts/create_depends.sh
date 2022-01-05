740d2e13c28ee08ccda9b2df54da439af8b7f26c
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2022-01-05 13:15:55 +0300

    add: first level headers to depend
    now utlis and tests make understand several changed headers
    
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- scripts/create_depends.sh
+++ scripts/create_depends.sh
@@ -2,15 +2,25 @@
 
 for var in "$@"
 do
-  _sources=$(gcc -I../includes -MM ${var}.c | tr -d '\\\n'|cut -d':' -f2|sed 's/\.\.\/includes\///g'|sed 's/\.h//g')
+  _depends=$(gcc -I../includes -MM ${var}.c | tr -d '\\\n'|cut -d':' -f2|sed 's/\.\.\/includes\///g')
+  _sources=$(echo $_depends|sed 's/\.h//g')
   for _s in $_sources;
   do
     _s=$(echo $_s|tr -d ' ')
     for _f in $(find ../cc -name "$_s*.c"|grep -v video);
     do
       _f=$(echo $_f|sed 's-\.\./cc/--g'|sed 's-\.c-\.o-g'|sed 's-\.xx\.o-\.xx_64\.o-g')
-      echo -e "../output/$var.bin: ../output/cc-local/$_f\n"
+      echo -e "../output/$var.bin: ../output/cc-local/$_f"
     done
+  done
+
+  _headers=$(echo $_depends|tr ' ' '\n'|grep -v ^$|grep -v ".c$"|sort|uniq)
+  for _h in $_headers;
+  do
+   _f=$(find ../includes|grep $_h|tr -d '\n')
+   if [[ "${_f}x" != "x" ]]; then
+     echo -e "$var.c: $_f"
+   fi
   done
   echo
 done
