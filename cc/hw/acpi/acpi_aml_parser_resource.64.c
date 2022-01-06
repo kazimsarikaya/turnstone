6967423a517f881f7180aa6db3f4b1c6a9d5add6
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2022-01-06 20:22:17 +0300

    add: parse crs
    acpi device's crs parsed and check both qemu and virtualbox
    
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- cc/hw/acpi/acpi_aml_parser_resource.64.c
+++ cc/hw/acpi/acpi_aml_parser_resource.64.c
@@ -4,4 +4,303 @@
  */
 
  #include <acpi/aml_internal.h>
+ #include <acpi/aml_resource.h>
  #include <video.h>
+
+int8_t acpi_aml_resource_parse_smallitem(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_smallitem_t* res);
+int8_t acpi_aml_resource_parse_smallitem_irq(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_smallitem_t* res);
+int8_t acpi_aml_resource_parse_smallitem_io(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_smallitem_t* res);
+int8_t acpi_aml_resource_parse_smallitem_dma(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_smallitem_t* res);
+int8_t acpi_aml_resource_parse_smallitem_endtag(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_smallitem_t* res);
+
+
+int8_t acpi_aml_resource_parse_largeitem(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_largeitem_t* res);
+int8_t acpi_aml_resource_parse_largeitem_memory_range_32bit_fixed(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_largeitem_t* res);
+int8_t acpi_aml_resource_parse_largeitem_extended_interrupt(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_largeitem_t* res);
+int8_t acpi_aml_resource_parse_largeitem_word_address_space(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_largeitem_t* res);
+int8_t acpi_aml_resource_parse_largeitem_dword_address_space(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_largeitem_t* res);
+int8_t acpi_aml_resource_parse_largeitem_qword_address_space(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_largeitem_t* res);
+
+
+
+int8_t acpi_aml_resource_parse_smallitem_irq(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_smallitem_t* res) {
+	if(device->interrupts == NULL) {
+		device->interrupts = linkedlist_create_list_with_heap(ctx->heap);
+	}
+
+	acpi_aml_device_interrupt_t* item = memory_malloc_ext(ctx->heap, sizeof(acpi_aml_device_interrupt_t), 0);
+
+	item->edge = res->irq.mode;
+	item->low = res->irq.polarity;
+	item->shared = res->irq.sharing;
+	item->wake_capability = res->irq.wake_capability;
+	item->interrupt_no = 0;
+
+	uint16_t intno = res->irq.irq_masks;
+
+	while((intno & 1) != 1) {
+		item->interrupt_no++;
+		intno >>= 1;
+	}
+
+	linkedlist_list_insert(device->interrupts, item);
+
+	return 0;
+}
+
+int8_t acpi_aml_resource_parse_smallitem_io(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_smallitem_t* res) {
+	if(device->ioports == NULL) {
+		device->ioports = linkedlist_create_list_with_heap(ctx->heap);
+	}
+
+	acpi_aml_device_ioport_t* item = memory_malloc_ext(ctx->heap, sizeof(acpi_aml_device_ioport_t), 0);
+
+	item->min = res->io.min;
+	item->max = res->io.max;
+
+	linkedlist_list_insert(device->ioports, item);
+
+	return 0;
+}
+
+int8_t acpi_aml_resource_parse_smallitem_dma(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_smallitem_t* res) {
+	if(device->dmas == NULL) {
+		device->dmas = linkedlist_create_list_with_heap(ctx->heap);
+	}
+
+	acpi_aml_device_dma_t* item = memory_malloc_ext(ctx->heap, sizeof(acpi_aml_device_dma_t), 0);
+
+	item->master = res->dma.bus_master_status;
+	item->channels = res->dma.channels;
+	item->speed = res->dma.speed_type;
+
+	linkedlist_list_insert(device->dmas, item);
+
+	return 0;
+}
+
+int8_t acpi_aml_resource_parse_smallitem_endtag(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_smallitem_t* res) {
+	UNUSED(ctx);
+	UNUSED(device);
+
+	return res->end_tag.checksum == 0?0:-1;
+}
+
+int8_t acpi_aml_resource_parse_largeitem_memory_range_32bit_fixed(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_largeitem_t* res) {
+	if(device->memory_ranges == NULL) {
+		device->memory_ranges = linkedlist_create_list_with_heap(ctx->heap);
+	}
+
+	acpi_aml_device_memory_range_t* item = memory_malloc_ext(ctx->heap, sizeof(acpi_aml_device_memory_range_t), 0);
+
+	item->writable = res->memory_range_32bit_fixed.rw;
+	item->cacheable = 0;
+	item->prefetchable = 0;
+	item->type = ACPI_AML_DEVICE_MEMORY_RANGE_MEMORY;
+	item->min = res->memory_range_32bit_fixed.base;
+	item->max = res->memory_range_32bit_fixed.base + res->memory_range_32bit_fixed.length;
+
+	linkedlist_list_insert(device->dmas, item);
+
+	return 0;
+}
+
+int8_t acpi_aml_resource_parse_largeitem_extended_interrupt(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_largeitem_t* res) {
+	if(device->interrupts == NULL) {
+		device->interrupts = linkedlist_create_list_with_heap(ctx->heap);
+	}
+
+	for(int32_t i = 0; i < res->extended_interrupt.count; i++) {
+		acpi_aml_device_interrupt_t* item = memory_malloc_ext(ctx->heap, sizeof(acpi_aml_device_interrupt_t), 0);
+
+		item->edge = res->extended_interrupt.mode;
+		item->low = res->extended_interrupt.polarity;
+		item->shared = res->extended_interrupt.sharing;
+		item->wake_capability = res->extended_interrupt.wake_capability;
+		item->interrupt_no =  res->extended_interrupt.interrupts[i];
+
+		linkedlist_list_insert(device->interrupts, item);
+	}
+
+	return 0;
+}
+
+int8_t acpi_aml_resource_parse_largeitem_word_address_space(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_largeitem_t* res) {
+	if(res->word_address_space.type == ACPI_AML_RESOURCE_WORD_ADDRESS_SPACE_TYPE_MEMORY) {
+		if(device->memory_ranges == NULL) {
+			device->memory_ranges = linkedlist_create_list_with_heap(ctx->heap);
+		}
+
+		acpi_aml_device_memory_range_t* item = memory_malloc_ext(ctx->heap, sizeof(acpi_aml_device_memory_range_t), 0);
+
+		item->writable = res->word_address_space.type_spesific_flags.memory_flag.write;
+		item->cacheable = (res->word_address_space.type_spesific_flags.memory_flag.mem & 1) == 1;
+		item->prefetchable = (res->word_address_space.type_spesific_flags.memory_flag.mem & 2) == 2;
+		item->type = res->word_address_space.type_spesific_flags.memory_flag.mtp;
+		item->min = res->word_address_space.min;
+		item->max = res->word_address_space.max;
+
+		linkedlist_list_insert(device->memory_ranges, item);
+	} else if(res->word_address_space.type == ACPI_AML_RESOURCE_WORD_ADDRESS_SPACE_TYPE_IO) {
+		if(device->ioports == NULL) {
+			device->ioports = linkedlist_create_list_with_heap(ctx->heap);
+		}
+
+		acpi_aml_device_ioport_t* item = memory_malloc_ext(ctx->heap, sizeof(acpi_aml_device_ioport_t), 0);
+
+		item->min = res->word_address_space.min;
+		item->max = res->word_address_space.max;
+
+		linkedlist_list_insert(device->ioports, item);
+	} else if(res->word_address_space.type == ACPI_AML_RESOURCE_WORD_ADDRESS_SPACE_TYPE_BUS) {
+		if(device->buses == NULL) {
+			device->buses = linkedlist_create_list_with_heap(ctx->heap);
+		}
+
+		acpi_aml_device_bus_t* item = memory_malloc_ext(ctx->heap, sizeof(acpi_aml_device_bus_t), 0);
+
+		item->min = res->word_address_space.min;
+		item->max = res->word_address_space.max;
+
+		linkedlist_list_insert(device->buses, item);
+	} else {
+		PRINTLOG(ACPIAML, LOG_ERROR, "device %s unknown word address type %i", device->name, res->word_address_space.type);
+
+		return -1;
+	}
+
+	return 0;
+}
+
+int8_t acpi_aml_resource_parse_largeitem_dword_address_space(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_largeitem_t* res) {
+	if(device->memory_ranges == NULL) {
+		device->memory_ranges = linkedlist_create_list_with_heap(ctx->heap);
+	}
+
+	acpi_aml_device_memory_range_t* item = memory_malloc_ext(ctx->heap, sizeof(acpi_aml_device_memory_range_t), 0);
+
+	item->writable = res->dword_address_space.type_spesific_flags.memory_flag.write;
+	item->cacheable = (res->dword_address_space.type_spesific_flags.memory_flag.mem & 1) == 1;
+	item->prefetchable = (res->dword_address_space.type_spesific_flags.memory_flag.mem & 2) == 2;
+	item->type = res->dword_address_space.type_spesific_flags.memory_flag.mtp;
+	item->min = res->dword_address_space.min;
+	item->max = res->dword_address_space.max;
+
+	linkedlist_list_insert(device->memory_ranges, item);
+
+	return 0;
+}
+
+int8_t acpi_aml_resource_parse_largeitem_qword_address_space(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_largeitem_t* res) {
+	if(device->memory_ranges == NULL) {
+		device->memory_ranges = linkedlist_create_list_with_heap(ctx->heap);
+	}
+
+	acpi_aml_device_memory_range_t* item = memory_malloc_ext(ctx->heap, sizeof(acpi_aml_device_memory_range_t), 0);
+
+	item->writable = res->qword_address_space.type_spesific_flags.memory_flag.write;
+	item->cacheable = (res->qword_address_space.type_spesific_flags.memory_flag.mem & 1) == 1;
+	item->prefetchable = (res->qword_address_space.type_spesific_flags.memory_flag.mem & 2) == 2;
+	item->type = res->qword_address_space.type_spesific_flags.memory_flag.mtp;
+	item->min = res->qword_address_space.min;
+	item->max = res->qword_address_space.max;
+
+	linkedlist_list_insert(device->memory_ranges, item);
+
+	return 0;
+}
+
+int8_t acpi_aml_resource_parse_smallitem(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_smallitem_t* res) {
+	int8_t mres = -1;
+
+	switch (res->name) {
+	case ACPI_AML_RESOURCE_SMALLITEM_IRQ:
+		mres = acpi_aml_resource_parse_smallitem_irq(ctx, device, res);
+		break;
+	case ACPI_AML_RESOURCE_SMALLITEM_IO:
+		mres = acpi_aml_resource_parse_smallitem_io(ctx, device, res);
+		break;
+	case ACPI_AML_RESOURCE_SMALLITEM_DMA:
+		mres = acpi_aml_resource_parse_smallitem_dma(ctx, device, res);
+		break;
+	case ACPI_AML_RESOURCE_SMALLITEM_ENDTAG:
+		mres = acpi_aml_resource_parse_smallitem_endtag(ctx, device, res);
+		break;
+	default:
+		PRINTLOG(ACPIAML, LOG_ERROR, "smallitem type is not implemented %i", res->name);
+		break;
+	}
+
+	return mres;
+}
+
+int8_t acpi_aml_resource_parse_largeitem(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_resource_largeitem_t* res) {
+	int8_t mres = -1;
+
+	switch (res->name) {
+	case ACPI_AML_RESOURCE_LARGEITEM_32BIT_FIXEDMEMORY_RANGE:
+		mres = acpi_aml_resource_parse_largeitem_memory_range_32bit_fixed(ctx, device, res);
+		break;
+	case ACPI_AML_RESOURCE_LARGEITEM_EXTENDED_INTERRUPT:
+		mres = acpi_aml_resource_parse_largeitem_extended_interrupt(ctx, device, res);
+		break;
+	case ACPI_AML_RESOURCE_LARGEITEM_WORD_ADDRESS_SPACE:
+		mres = acpi_aml_resource_parse_largeitem_word_address_space(ctx, device, res);
+		break;
+	case ACPI_AML_RESOURCE_LARGEITEM_DWORD_ADDRESS_SPACE:
+		mres = acpi_aml_resource_parse_largeitem_dword_address_space(ctx, device, res);
+		break;
+	case ACPI_AML_RESOURCE_LARGEITEM_QWORD_ADDRESS_SPACE:
+		mres = acpi_aml_resource_parse_largeitem_qword_address_space(ctx, device, res);
+		break;
+	default:
+		PRINTLOG(ACPIAML, LOG_ERROR, "largeitem type is not implemented %i", res->name);
+		break;
+	}
+
+	return mres;
+}
+
+int32_t acpi_aml_resource_parse(acpi_aml_parser_context_t* ctx, acpi_aml_device_t* device, acpi_aml_object_t* buffer) {
+	if(ctx == NULL) {
+		PRINTLOG(ACPIAML, LOG_ERROR, "acpi ctx is null", 0);
+		return -1;
+	}
+
+	if(buffer == NULL) {
+		PRINTLOG(ACPIAML, LOG_ERROR, "buffer is null", 0);
+		return -1;
+	}
+
+	if(buffer->type != ACPI_AML_OT_BUFFER) {
+		PRINTLOG(ACPIAML, LOG_ERROR, "buffer type mismatch", 0);
+		return -1;
+	}
+
+	int64_t crs_size = buffer->buffer.buflen;
+	int64_t offset = 0;
+	int64_t itemlen = 0;
+	acpi_aml_resource_t* res = (acpi_aml_resource_t*)buffer->buffer.buf;
+
+	int32_t parse_res = 0;
+
+	while(offset < crs_size) {
+		if(res->identifier.type == ACPI_AML_RESOURCE_SMALLITEM) {
+			PRINTLOG(ACPIAML, LOG_TRACE, "device %s small item %i with length %i", device->name, res->smallitem.name, res->smallitem.length);
+			itemlen = res->smallitem.length + 1;
+			parse_res += acpi_aml_resource_parse_smallitem(ctx, device, (acpi_aml_resource_smallitem_t*)res);
+		} else if (res->identifier.type == ACPI_AML_RESOURCE_LARGEITEM) {
+			PRINTLOG(ACPIAML, LOG_TRACE, "device %s large item %i with length %i", device->name, res->largeitem.name, res->largeitem.length);
+			itemlen = res->largeitem.length + 3;
+			parse_res += acpi_aml_resource_parse_largeitem(ctx, device, (acpi_aml_resource_largeitem_t*)res);
+		} else {
+			PRINTLOG(ACPIAML, LOG_ERROR, "device %s unknown crs type %i, stopping enumaration", device->name, res->identifier.type);
+			break;
+		}
+
+		offset += itemlen;
+		res = (acpi_aml_resource_t*)(buffer->buffer.buf + offset);
+	}
+
+	return parse_res;
+}
--- includes/acpi/aml.h
+++ includes/acpi/aml.h
@@ -112,329 +112,45 @@ typedef struct _acpi_aml_object_t {
 	};
 }acpi_aml_object_t;
 
-typedef enum {
-	ACPI_AML_RESOURCE_SMALLITEM=0,
-	ACPI_AML_RESOURCE_LARGEITEM,
-}acpi_aml_resource_type_t;
+typedef struct {
+	uint16_t min;
+	uint16_t max;
+}acpi_aml_device_bus_t;
+
+typedef struct {
+	uint16_t min;
+	uint16_t max;
+}acpi_aml_device_ioport_t;
+
+typedef struct {
+	boolean_t master;
+	uint8_t channels;
+	uint8_t speed;
+}acpi_aml_device_dma_t;
+
+typedef struct {
+	boolean_t edge;
+	boolean_t low;
+	boolean_t shared;
+	boolean_t wake_capability;
+	uint32_t interrupt_no;
+}acpi_aml_device_interrupt_t;
 
 typedef enum {
-	ACPI_AML_RESOURCE_SMALLITEM_IRQ=4,
-	ACPI_AML_RESOURCE_SMALLITEM_DMA=5,
-	ACPI_AML_RESOURCE_SMALLITEM_START_DEPFUNC=6,
-	ACPI_AML_RESOURCE_SMALLITEM_END_DEPFUNC=7,
-	ACPI_AML_RESOURCE_SMALLITEM_IO=8,
-	ACPI_AML_RESOURCE_SMALLITEM_FIXEDIO=9,
-	ACPI_AML_RESOURCE_SMALLITEM_FIXEDDMA=10,
-	ACPI_AML_RESOURCE_SMALLITEM_VENDOR=14,
-	ACPI_AML_RESOURCE_SMALLITEM_ENDTAG=15,
-}acpi_aml_resource_smallitem_name_t;
+	ACPI_AML_DEVICE_MEMORY_RANGE_MEMORY,
+	ACPI_AML_DEVICE_MEMORY_RANGE_RESERVED,
+	ACPI_AML_DEVICE_MEMORY_RANGE_ACPI,
+	ACPI_AML_DEVICE_MEMORY_RANGE_NVS,
+}acpi_aml_device_memory_range_type_t;
 
-typedef enum {
-	ACPI_AML_RESOURCE_LARGEITEM_24BIT_MEMORY_RANGE=1,
-	ACPI_AML_RESOURCE_LARGEITEM_GENERIC_REGISTER=2,
-	ACPI_AML_RESOURCE_LARGEITEM_VENDOR=4,
-	ACPI_AML_RESOURCE_LARGEITEM_32BIT_MEMORY_RANGE=5,
-	ACPI_AML_RESOURCE_LARGEITEM_32BIT_FIXEDMEMORY_RANGE=6,
-	ACPI_AML_RESOURCE_LARGEITEM_DWORDADDRESS_SPACE=7,
-	ACPI_AML_RESOURCE_LARGEITEM_WORD_ADDRESS_SPACE=8,
-	ACPI_AML_RESOURCE_LARGEITEM_EXTENDED_INTERRUPT=9,
-	ACPI_AML_RESOURCE_LARGEITEM_QWORD_ADDRESS_SPACE=10,
-	ACPI_AML_RESOURCE_LARGEITEM_EXTENDED_ADDRESS_SPACE=11,
-	ACPI_AML_RESOURCE_LARGEITEM_GPIO_CONNECTION=12,
-	ACPI_AML_RESOURCE_LARGEITEM_PIN_FUNCTION=13,
-	ACPI_AML_RESOURCE_LARGEITEM_GENERIC_SERIAL_BUS_CONNECTION=14,
-	ACPI_AML_RESOURCE_LARGEITEM_PIN_CONFIGURATION=15,
-	ACPI_AML_RESOURCE_LARGEITEM_PIN_GROUP=16,
-	ACPI_AML_RESOURCE_LARGEITEM_PIN_GROUP_FUNCTION=17,
-	ACPI_AML_RESOURCE_LARGEITEM_PIN_GROUP_CONFIGURATION=18,
-}acpi_aml_resource_largeitem_name_t;
-
-typedef enum {
-	ACPI_AML_RESOURCE_WORD_ADDRESS_SPACE_TYPE_MEMORY=0,
-	ACPI_AML_RESOURCE_WORD_ADDRESS_SPACE_TYPE_IO=1,
-	ACPI_AML_RESOURCE_WORD_ADDRESS_SPACE_TYPE_BUS=2,
-}acpi_aml_resource_word_address_space_type_t;
-
-typedef enum {
-	ACPI_AML_RESOURCE_ADDRESS_SPACE_ID_MEMORY=0,
-	ACPI_AML_RESOURCE_ADDRESS_SPACE_ID_IO=1,
-	ACPI_AML_RESOURCE_ADDRESS_SPACE_ID_PCI_CONF_SPACE=2,
-	ACPI_AML_RESOURCE_ADDRESS_SPACE_ID_EMBEDED_CONTROLLER=3,
-	ACPI_AML_RESOURCE_ADDRESS_SPACE_ID_SMBUS=4,
-	ACPI_AML_RESOURCE_ADDRESS_SPACE_ID_CMOS=5,
-	ACPI_AML_RESOURCE_ADDRESS_SPACE_ID_PCI_BAR_TARGET=6,
-	ACPI_AML_RESOURCE_ADDRESS_SPACE_ID_IPMI=7,
-	ACPI_AML_RESOURCE_ADDRESS_SPACE_ID_GPIO=8,
-	ACPI_AML_RESOURCE_ADDRESS_SPACE_ID_SERIALBUS=9,
-	ACPI_AML_RESOURCE_ADDRESS_SPACE_ID_PCC=10,
-	ACPI_AML_RESOURCE_ADDRESS_SPACE_ID_FUNCTIONAL_FIXED_HARDWARE=0X7F,
-} acpi_aml_resource_address_space_id_t;
-
-typedef enum {
-	ACPI_AML_RESOURCE_ACCESS_SIZE_UNDEFINED=0,
-	ACPI_AML_RESOURCE_ACCESS_SIZE_BYTE=1,
-	ACPI_AML_RESOURCE_ACCESS_SIZE_WORD=2,
-	ACPI_AML_RESOURCE_ACCESS_SIZE_DWORD=3,
-	ACPI_AML_RESOURCE_ACCESS_SIZE_QWORD=4,
-}acpi_aml_resource_access_size_t;
-
-typedef union {
-	struct {
-		uint8_t write : 1;
-		uint8_t mem : 2;
-		uint8_t mtp : 2;
-		uint8_t ttp : 1;
-		uint8_t reserved : 2;
-	}__attribute__((packed)) memory_resource_flag;
-
-	struct {
-		uint8_t rng : 2;
-		uint8_t reserved1 : 2;
-		uint8_t ttp : 1;
-		uint8_t trs : 1;
-		uint8_t reserved2 : 2;
-	}__attribute__((packed)) io_resource_flag;
-
-	struct {
-		uint8_t reserved;
-	}__attribute__((packed)) bus_number_resource_flag;
-
-}__attribute__((packed)) acpi_aml_resource_type_specific_flag_t;
-
-typedef union {
-	struct {
-		uint8_t unused : 7;
-		acpi_aml_resource_type_t type : 1;
-	}__attribute__((packed)) type;
-
-	struct {
-		uint8_t length : 3;
-		acpi_aml_resource_smallitem_name_t name : 4;
-		acpi_aml_resource_type_t type : 1;
-
-		union {
-			struct {
-				uint16_t irq_masks : 16;
-				uint8_t mode : 1;
-				uint8_t ignored : 2;
-				uint8_t polarity : 1;
-				uint8_t sharing : 1;
-				uint8_t wake_capability : 1;
-				uint8_t reserved : 2;
-			}__attribute__((packed)) irq;
-
-			struct {
-				uint8_t channels : 8;
-				uint8_t size : 2;
-				uint8_t bus_master_status : 1;
-				uint8_t ignored : 2;
-				uint8_t speed_type : 2;
-				uint8_t zero : 1;
-			}__attribute__((packed)) dma;
-
-			struct {
-				uint8_t compability : 2;
-				uint8_t performance : 2;
-				uint8_t reserved : 4;
-			}__attribute__((packed)) start_depfunc;
-
-			struct {
-				uint8_t decode : 1;
-				uint8_t unused : 7;
-				uint16_t min : 16;
-				uint16_t max : 16;
-				uint8_t align : 8;
-				uint8_t length : 8;
-			}__attribute__((packed)) io;
-
-			struct {
-				uint16_t base : 10;
-				uint8_t unused : 6;
-				uint16_t length : 16;
-			}__attribute__((packed)) fixedio;
-
-			struct {
-				uint16_t request_line : 16;
-				uint16_t channel : 16;
-				uint8_t size : 8;
-			}__attribute__((packed)) fixeddma;
-
-			struct {
-				uint8_t checksum : 8;
-			}__attribute__((packed)) end_tag;
-
-		}__attribute__((packed));
-
-	}__attribute__((packed)) smallitem;
-
-	struct {
-		acpi_aml_resource_largeitem_name_t name : 7;
-		acpi_aml_resource_type_t type : 1;
-		uint16_t length : 16;
-
-		union {
-			struct {
-				uint8_t readwrite : 1;
-				uint8_t ignored : 7;
-				uint16_t min : 16;
-				uint16_t max : 16;
-				uint16_t align : 16;
-				uint16_t length : 16;
-			}__attribute__((packed)) memory_range_24bit;
-
-			struct {
-				uint8_t uuid_sub_type;
-				uint8_t uuid[16];
-				uint8_t vendor_data[0];
-			}__attribute__((packed)) vendor;
-
-			struct {
-				uint8_t rw : 1;
-				uint8_t ignored : 7;
-				uint32_t min;
-				uint32_t max;
-				uint32_t align;
-				uint32_t length;
-			}__attribute__((packed)) memory_range_32bit;
-
-			struct {
-				uint8_t rw : 1;
-				uint8_t ignored : 7;
-				uint32_t base;
-				uint32_t length;
-			}__attribute__((packed)) memory_range_32bit_fixed;
-
-			struct {
-				acpi_aml_resource_word_address_space_type_t type : 8;
-				uint8_t ignored : 1;
-				uint8_t decode_type : 1;
-				uint8_t min_address_fixed : 1;
-				uint8_t max_address_fixed : 1;
-				uint8_t reserved : 4;
-				acpi_aml_resource_type_specific_flag_t type_spesific_flags;
-				uint16_t gra;
-				uint16_t min;
-				uint16_t max;
-				uint16_t translation_offset;
-				uint16_t length;
-				uint8_t resource_source_index;
-				char_t resource_source[0];
-			}__attribute__((packed)) word_address_space;
-
-			struct {
-				uint8_t ignored : 1;
-				uint8_t decode_type : 1;
-				uint8_t min_address_fixed : 1;
-				uint8_t max_address_fixed : 1;
-				uint8_t reserved : 4;
-				acpi_aml_resource_type_specific_flag_t type_spesific_flags;
-				uint32_t gra;
-				uint32_t min;
-				uint32_t max;
-				uint32_t translation_offset;
-				uint32_t length;
-				uint8_t resource_source_index;
-				char_t resource_source[0];
-			}__attribute__((packed)) dword_address_space;
-
-			struct {
-				uint8_t ignored : 1;
-				uint8_t decode_type : 1;
-				uint8_t min_address_fixed : 1;
-				uint8_t max_address_fixed : 1;
-				uint8_t reserved : 4;
-				acpi_aml_resource_type_specific_flag_t type_spesific_flags;
-				uint64_t gra;
-				uint64_t min;
-				uint64_t max;
-				uint64_t translation_offset;
-				uint64_t length;
-				uint8_t resource_source_index;
-				char_t resource_source[0];
-			}__attribute__((packed)) qword_address_space;
-
-			struct {
-				uint8_t ignored : 1;
-				uint8_t decode_type : 1;
-				uint8_t min_address_fixed : 1;
-				uint8_t max_address_fixed : 1;
-				uint8_t reserved : 4;
-				acpi_aml_resource_type_specific_flag_t type_spesific_flags;
-				uint64_t gra;
-				uint64_t min;
-				uint64_t max;
-				uint64_t translation_offset;
-				uint64_t length;
-				uint8_t type_spesific_attribute;
-				uint64_t attribute;
-			}__attribute__((packed)) extended_address_space;
-
-			struct {
-				uint8_t consumer : 1;
-				uint8_t mode : 1;
-				uint8_t polarity : 1;
-				uint8_t sharing : 1;
-				uint8_t wake_capability : 1;
-				uint8_t reserved : 3;
-				uint8_t length;
-				// extra attributes length X 32bit interrupt number resource source index and its data
-			}__attribute__((packed)) extended_interrupt;
-
-			struct {
-				acpi_aml_resource_address_space_id_t asi : 8;
-				uint8_t register_bit_width;
-				uint8_t register_bit_offset;
-				acpi_aml_resource_access_size_t access_size : 8;
-				uint64_t address;
-			}__attribute__((packed)) generic_register;
-
-			struct {
-				uint8_t data[0];
-			}__attribute__((packed)) gpio_descriptor;
-
-			struct {
-				uint8_t data[0];
-			}__attribute__((packed)) generic_serial_bus;
-
-			struct {
-				uint8_t data[0];
-			}__attribute__((packed)) pin_function;
-
-			struct {
-				uint8_t data[0];
-			}__attribute__((packed)) pin_configuration;
-
-			struct {
-				uint8_t data[0];
-			}__attribute__((packed)) pin_group;
-
-			struct {
-				uint8_t revision_id : 8;
-				uint16_t flags : 16;
-				uint16_t function : 16;
-				uint8_t source_index : 8;
-				uint16_t source_name_offset : 16;
-				uint16_t source_label_offset : 16;
-				uint16_t vendor_data_offset : 16;
-				uint16_t vendor_data_length : 16;
-			}__attribute__((packed)) pin_group_function;
-
-			struct {
-				uint8_t revision_id : 8;
-				uint16_t flags : 16;
-				uint8_t type : 8;
-				uint32_t value : 32;
-				uint8_t source_index : 8;
-				uint16_t source_name_offset : 16;
-				uint16_t source_label_offset : 16;
-				uint16_t vendor_data_length : 16;
-			}__attribute__((packed)) pin_group_configuration;
-
-		}__attribute__((packed));
-
-	}__attribute__((packed)) largeitem;
-
-}__attribute__((packed)) acpi_aml_resource_t;
+typedef struct {
+	boolean_t writable;
+	boolean_t cacheable;
+	boolean_t prefetchable;
+	acpi_aml_device_memory_range_type_t type;
+	uint64_t min;
+	uint64_t max;
+}acpi_aml_device_memory_range_t;
 
 typedef struct acpi_aml_device {
 	char_t* name;
@@ -449,6 +165,11 @@ typedef struct acpi_aml_device {
 	acpi_aml_object_t* srs;
 	acpi_aml_object_t* sta;
 	acpi_aml_object_t* uid;
+	linkedlist_t buses;
+	linkedlist_t ioports;
+	linkedlist_t dmas;
+	linkedlist_t memory_ranges;
+	linkedlist_t interrupts;
 }acpi_aml_device_t;
 
 typedef struct {
@@ -487,6 +208,10 @@ acpi_aml_parser_context_t* acpi_aml_parser_context_create_with_heap(memory_heap_
 void acpi_aml_parser_context_destroy(acpi_aml_parser_context_t*);
 
 int8_t acpi_aml_parse(acpi_aml_parser_context_t*);
+
+acpi_aml_object_t* acpi_aml_symbol_lookup(acpi_aml_parser_context_t*, char_t*);
+int8_t acpi_aml_read_as_integer(acpi_aml_parser_context_t*, acpi_aml_object_t*, int64_t*);
+int8_t acpi_aml_write_as_integer(acpi_aml_parser_context_t*, int64_t, acpi_aml_object_t*);
 
 int8_t acpi_device_build(acpi_aml_parser_context_t*);
 int8_t acpi_device_init(acpi_aml_parser_context_t*);
