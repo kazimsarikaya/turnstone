0fc520137fb04a4e5018b0b1660c510d61af592d
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2022-01-23 18:45:28 +0300

    add: ps/2 and virtio-kbd input support
    for virtual box ps/2 handler implemented.
    
    for qemu virtio-input driver implemented only for handling keyboard
    events.
    
    keyboard key buffer isnot present, hence we cannot send keys to any
    destination. only echo printable chars to the screen and serial console.
    
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- includes/device/kbd.h
+++ includes/device/kbd.h
@@ -12,7 +12,16 @@
 
 #include <types.h>
 #include <cpu/interrupt.h>
+#include <pci.h>
+#include <driver/virtio.h>
+
+#define KBD_DATA_PORT 0x60
+
+#define KBD_DEVICE_VENDOR_ID_VIRTIO  0x1AF4
+#define KBD_DEVICE_DEVICE_ID_VIRTIO  0x1052
+
+int8_t kbd_init();
 
 int8_t dev_kbd_isr(interrupt_frame_t* frame, uint8_t intnum);
-
+int8_t dev_virtio_kbd_init();
 #endif
---
+++ includes/device/kbd_scancodes.h
@@ -0,0 +1,75 @@
+/*
+ * This work is licensed under TURNSTONE OS Public License.
+ * Please read and understand latest version of Licence.
+ */
+
+#ifndef ___KBD_SCANCODES_H
+#define ___KBD_SCANCODES_H 0
+
+#include <types.h>
+
+#define KBD_PS2_EXTCODE_PREFIX    0xE0
+
+#define KBD_SCANCODE_NULL         0x00
+#define KBD_SCANCODE_ESC          0x01
+#define KBD_SCANCODE_BACKSPACE    0x0E
+
+#define KBD_SCANCODE_LEFTCTRL     0x1D
+#define KBD_SCANCODE_RIGHTTCTRL   0x61
+
+#define KBD_SCANCODE_LEFTSHIFT    0x2A
+#define KBD_SCANCODE_RIGHTSHIFT   0x36
+
+#define KBD_SCANCODE_LEFTALT      0x38
+#define KBD_SCANCODE_RIGHTALT     0x64
+
+#define KBD_SCANCODE_CAPSLOCK     0x3A
+
+#define KBD_SCANCODE_LEFTMETA     0x7D
+#define KBD_SCANCODE_RIGHTMETA    0x7F
+
+#define KBD_SCANCODE_F1           0x3B
+#define KBD_SCANCODE_F2           0x3C
+#define KBD_SCANCODE_F3           0x3D
+#define KBD_SCANCODE_F4           0x3E
+#define KBD_SCANCODE_F5           0x3F
+#define KBD_SCANCODE_F6           0x40
+#define KBD_SCANCODE_F7           0x41
+#define KBD_SCANCODE_F8           0x42
+#define KBD_SCANCODE_F9           0x43
+#define KBD_SCANCODE_F10          0x44
+#define KBD_SCANCODE_F11          0x57
+#define KBD_SCANCODE_F12          0x58
+
+#define KBD_SCANCODE_NUMLOCK      0x45
+#define KBD_SCANCODE_SCROLLLOCK   0x46
+
+#define KBD_SCANCODE_HOME         0x66
+#define KBD_SCANCODE_UP           0x67
+#define KBD_SCANCODE_PAGEUP       0x68
+#define KBD_SCANCODE_LEFT         0x69
+#define KBD_SCANCODE_RIGHT        0x6A
+#define KBD_SCANCODE_END          0x6B
+#define KBD_SCANCODE_DOWN         0x6C
+#define KBD_SCANCODE_PAGEDOWN     0x6D
+#define KBD_SCANCODE_INSERT       0x6E
+#define KBD_SCANCODE_DELETE       0x6F
+
+
+extern wchar_t KBD_SCANCODES_NORMAL[];
+extern wchar_t KBD_SCANCODES_CAPSON[];
+extern wchar_t KBD_SCANCODES_SHIFT[];
+extern wchar_t KBD_SCANCODES_SHIFTCAPSON[];
+extern wchar_t KBD_SCANCODES_ALT[];
+extern wchar_t KBD_SCANCODES_ALTSHIFT[];
+
+typedef struct kbd_state_s {
+	boolean_t is_capson;
+	boolean_t is_shift_pressed;
+	boolean_t is_alt_pressed;
+}kbd_state_t;
+
+wchar_t kbd_scancode_get_value(wchar_t scancode, kbd_state_t* ks, boolean_t* is_printable);
+wchar_t kbd_scancode_fixcode(wchar_t ps2code);
+
+#endif
