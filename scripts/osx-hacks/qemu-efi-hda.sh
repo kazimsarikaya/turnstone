7cb5f1198047ec55ed670fcf498b589810081a0e
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2022-02-16 17:14:04 +0300

    add: cpu flags, nvme disk
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- scripts/osx-hacks/qemu-efi-hda.sh
+++ scripts/osx-hacks/qemu-efi-hda.sh
@@ -27,10 +27,12 @@ fi
 
 qemu-system-x86_64 \
   -M q35 -m 1g -smp cpus=2 -name osdev-hda-boot \
-  -cpu qemu64,+ssse3,+sse4.1,+sse4.2 \
+  -cpu max \
   -drive if=pflash,readonly=on,format=raw,unit=0,file=${CURRENTDIR}/edk2-x86_64-code.fd \
   -drive if=pflash,readonly=off,format=raw,unit=1,file=${CURRENTDIR}/edk2-i386-vars.fd \
   -drive index=0,media=disk,format=raw,file=${OUTPUTDIR}/qemu-hda,werror=report,rerror=report \
+  -drive id=cache,if=none,format=raw,file=${OUTPUTDIR}/qemu-nvme-cache,werror=report,rerror=report \
+  -device nvme,drive=cache,serial=qn0001,id=nvme0 \
   -monitor stdio \
   -device vmware-svga,id=gpu0 \
   -device virtio-net,netdev=t0,id=nic0 \
