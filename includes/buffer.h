43b776e6656c669d82ecc8e03ec5f8f46215a60a
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2022-02-22 00:27:23 +0300

    add: buffer
    a dynmaic byte buffer container implemented
    
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

---
+++ includes/buffer.h
@@ -0,0 +1,26 @@
+/**
+ * @file buffer.h
+ * @brief buffer interface
+ *
+ * This work is licensed under TURNSTONE OS Public License.
+ * Please read and understand latest version of Licence.
+ */
+
+#ifndef ___BUFFER_H
+#define ___BUFFER_H 0
+
+#include <types.h>
+#include <memory.h>
+
+typedef void* buffer_t;
+
+buffer_t buffer_new_with_capacity(memory_heap_t* heap, uint64_t capacity);
+#define buffer_new() buffer_new_with_capacity(NULL, 128)
+
+buffer_t buffer_append_byte(buffer_t buffer, uint8_t data);
+buffer_t buffer_append_bytes(buffer_t buffer, uint8_t* data, uint64_t length);
+uint8_t* buffer_get_bytes(buffer_t buffer, uint64_t* length);
+int8_t buffer_destroy(buffer_t buffer);
+
+
+#endif
