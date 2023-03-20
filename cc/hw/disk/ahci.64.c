caad1f12b9443d8446eda7fa0087094e88c8c848
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2023-03-20 17:57:21 +0000

    update: dox header
    update dox header
    
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- cc/hw/disk/ahci.64.c
+++ cc/hw/disk/ahci.64.c
@@ -1,4 +1,7 @@
 /*
+ * @file ahci.64.c
+ * @brief ahci interface implentation.
+ *
  * This work is licensed under TURNSTONE OS Public License.
  * Please read and understand latest version of Licence.
  */
