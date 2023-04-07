442bb124e9fb0b91f0221960aa1d72f68d269e6f
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2023-04-07 14:18:22 +0000

    del: cleanup unneccessary files
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- cc/tosdb/tosdb_valuelog.64.c
+++
@@ -1,9 +0,0 @@
-/**
- * @file tosdb_valuelog.64.c
- * @brief tosdb valuelog interface implementation
- *
- * This work is licensed under TURNSTONE OS Public License.
- * Please read and understand latest version of Licence.
- */
-
-#include <tosdb/valuelog.h>
