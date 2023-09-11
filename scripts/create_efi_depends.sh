8c017408a65db253c7ad43a77c3febe3abc83043
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2023-09-11 08:09:01 +0000

    fix: header deps.
    when a header changes efi binary not builded. we partially solved that.
    
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- scripts/create_efi_depends.sh
+++ scripts/create_efi_depends.sh
@@ -9,25 +9,29 @@ shift
 
 DEPEND_OBJS=""
 
-for _param in $@;
-do
+for _param in $@; do
   _sources=$(gcc -I../includes -MM $_param | tr -d '\\\n'|cut -d':' -f2|sed 's/\.\.\/includes\///g'|sed 's/\.h//g')
-  for _s in $_sources;
-  do
+
+  for _s in $_sources; do
     _s=$(basename $_s|tr -d ' ')
-    for _f in $(find ../cc -name "$_s*.c"|grep -v video);
-    do
+
+    for _f in $(find ../cc -name "$_s*.c"|grep -v video); do
       _f=$(echo $_f|sed 's-\.\./cc/--g'|sed 's-\.c-\.o-g'|sed 's-\.xx\.o-\.xx_64\.o-g')
       DEPEND_OBJS="$DEPEND_OBJS\n$_f"
     done
+
   done
-  echo
+
 done
 
 DEPEND_OBJS=$(echo -e $DEPEND_OBJS|sort|uniq)
 
-for _f in $DEPEND_OBJS;
-do
+for _f in $DEPEND_OBJS; do
+  _s=$(basename $_f|sed 's/\..*$//g')
+  echo -e ../output/efi/$_f: $(find ../includes -name "$_s*.h") 
+  echo 
   echo -e "$APP: ../output/efi/$_f"
+  echo
   echo -e "EFIOBJS += \$(EFIOUTPUT)/$_f"
+  echo
 done
