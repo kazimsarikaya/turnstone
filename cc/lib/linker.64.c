3694d926b8a1b036e45488df3d6666879127a536
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2024-10-24 22:36:50 +0000

    fix: double destroy
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- cc/lib/linker.64.c
+++ cc/lib/linker.64.c
@@ -409,7 +409,6 @@ int8_t linker_build_relocations(linker_context_t* ctx, uint64_t section_id, uint
 
         if(!reloc_rec->get_int64(reloc_rec, "id", &reloc_id)) {
             PRINTLOG(LINKER, LOG_ERROR, "cannot get relocation id");
-            reloc_rec->destroy(reloc_rec);
 
             goto clean_relocs_iter;
         }
@@ -420,14 +419,12 @@ int8_t linker_build_relocations(linker_context_t* ctx, uint64_t section_id, uint
 
         if(!reloc_rec->get_int64(reloc_rec, "symbol_section_id", &symbol_section_id)) {
             PRINTLOG(LINKER, LOG_ERROR, "cannot get relocation symbol section id for relocation id 0x%llx", reloc_id);
-            reloc_rec->destroy(reloc_rec);
 
             goto clean_relocs_iter;
         }
 
         if(!reloc_rec->get_string(reloc_rec, "symbol_name", &symbol_name)) {
             PRINTLOG(LINKER, LOG_ERROR, "cannot get relocation symbol name for relocation id 0x%llx", reloc_id);
-            reloc_rec->destroy(reloc_rec);
 
             goto clean_relocs_iter;
         }
@@ -443,7 +440,6 @@ int8_t linker_build_relocations(linker_context_t* ctx, uint64_t section_id, uint
 
         if(symbol_id_missing && !is_got_symbol) {
             PRINTLOG(LINKER, LOG_ERROR, "symbol id is missing for symbol %s, relocation at section 0x%llx relocation id 0x%llx", symbol_name, section_id, reloc_id);
-            reloc_rec->destroy(reloc_rec);
             memory_free(symbol_name);
 
             goto clean_relocs_iter;
@@ -453,8 +449,6 @@ int8_t linker_build_relocations(linker_context_t* ctx, uint64_t section_id, uint
             if(!is_got_symbol) {
                 PRINTLOG(LINKER, LOG_ERROR, "symbol section id is missing for symbol %s(%lli), relocation at section 0x%llx relocation id 0x%llx", symbol_name, symbol_id, section_id, reloc_id);
                 PRINTLOG(LINKER, LOG_ERROR, "relocation record deleted? %s", reloc_rec->is_deleted(reloc_rec) ? "yes" : "no");
-
-                reloc_rec->destroy(reloc_rec);
 
                 memory_free(symbol_name);
 
@@ -468,21 +462,18 @@ int8_t linker_build_relocations(linker_context_t* ctx, uint64_t section_id, uint
 
         if(!reloc_rec->get_int8(reloc_rec, "type", &reloc_type)) {
             PRINTLOG(LINKER, LOG_ERROR, "cannot get relocation type for relocation id 0x%llx", reloc_id);
-            reloc_rec->destroy(reloc_rec);
 
             goto clean_relocs_iter;
         }
 
         if(!reloc_rec->get_int64(reloc_rec, "offset", &reloc_offset)) {
             PRINTLOG(LINKER, LOG_ERROR, "cannot get relocation offset for relocation id 0x%llx", reloc_id);
-            reloc_rec->destroy(reloc_rec);
 
             goto clean_relocs_iter;
         }
 
         if(!reloc_rec->get_int64(reloc_rec, "addend", &reloc_addend)) {
             PRINTLOG(LINKER, LOG_ERROR, "cannot get relocation addend for relocation id 0x%llx", reloc_id);
-            reloc_rec->destroy(reloc_rec);
 
             goto clean_relocs_iter;
         }
@@ -492,14 +483,12 @@ int8_t linker_build_relocations(linker_context_t* ctx, uint64_t section_id, uint
 
             if(!s_sec_rec) {
                 PRINTLOG(LINKER, LOG_ERROR, "cannot create record for searching section");
-                reloc_rec->destroy(reloc_rec);
 
                 goto clean_relocs_iter;
             }
 
             if(!s_sec_rec->set_uint64(s_sec_rec, "id", symbol_section_id)) {
                 PRINTLOG(LINKER, LOG_ERROR, "cannot set search key for records id column for section id 0x%llx", symbol_section_id);
-                reloc_rec->destroy(reloc_rec);
                 s_sec_rec->destroy(s_sec_rec);
 
                 goto clean_relocs_iter;
@@ -507,7 +496,6 @@ int8_t linker_build_relocations(linker_context_t* ctx, uint64_t section_id, uint
 
             if(!s_sec_rec->get_record(s_sec_rec)) {
                 PRINTLOG(LINKER, LOG_ERROR, "cannot get section record for section id 0x%llx for relocation 0x%llx", symbol_section_id, reloc_id);
-                reloc_rec->destroy(reloc_rec);
                 s_sec_rec->destroy(s_sec_rec);
 
                 goto clean_relocs_iter;
@@ -515,7 +503,6 @@ int8_t linker_build_relocations(linker_context_t* ctx, uint64_t section_id, uint
 
             if(!s_sec_rec->get_int64(s_sec_rec, "module_id", &module_id)) {
                 PRINTLOG(LINKER, LOG_ERROR, "cannot get section module id, is deleted? %d", s_sec_rec->is_deleted(s_sec_rec));
-                reloc_rec->destroy(reloc_rec);
                 s_sec_rec->destroy(s_sec_rec);
 
                 goto clean_relocs_iter;
@@ -535,7 +522,6 @@ int8_t linker_build_relocations(linker_context_t* ctx, uint64_t section_id, uint
 
                 if(!module->plt_offsets) {
                     PRINTLOG(LINKER, LOG_ERROR, "cannot create plt offsets hashmap");
-                    reloc_rec->destroy(reloc_rec);
 
                     goto clean_relocs_iter;
                 }
@@ -547,7 +533,6 @@ int8_t linker_build_relocations(linker_context_t* ctx, uint64_t section_id, uint
 
                 if(!plt_section->section_data) {
                     PRINTLOG(LINKER, LOG_ERROR, "cannot create plt section data buffer");
-                    reloc_rec->destroy(reloc_rec);
 
                     goto clean_relocs_iter;
                 }
@@ -661,7 +646,6 @@ int8_t linker_build_relocations(linker_context_t* ctx, uint64_t section_id, uint
 
                     if( recursive_res == -1) {
                         PRINTLOG(LINKER, LOG_ERROR, "cannot build module for got symbol 0x%llx module 0x%llx", symbol_id, module_id);
-                        reloc_rec->destroy(reloc_rec);
 
                         goto clean_relocs_iter;
                     } else if(recursive_res == -2) {
@@ -671,7 +655,6 @@ int8_t linker_build_relocations(linker_context_t* ctx, uint64_t section_id, uint
 
                         if(!got_index) {
                             PRINTLOG(LINKER, LOG_ERROR, "cannot get got index for symbol 0x%llx after recursive loading", symbol_id);
-                            reloc_rec->destroy(reloc_rec);
 
                             goto clean_relocs_iter;
                         } else {
@@ -843,28 +826,24 @@ int8_t linker_build_module(linker_context_t* ctx, uint64_t module_id, boolean_t 
 
         if(!sec_rec->get_uint64(sec_rec, "id", &section_id)) {
             PRINTLOG(LINKER, LOG_ERROR, "cannot get section id");
-            sec_rec->destroy(sec_rec);
 
             goto clean_secs_iter;
         }
 
         if(!sec_rec->get_uint8(sec_rec, "type", &section_type)) {
             PRINTLOG(LINKER, LOG_ERROR, "cannot get section type");
-            sec_rec->destroy(sec_rec);
 
             goto clean_secs_iter;
         }
 
         if(!sec_rec->get_uint64(sec_rec, "size", &section_size)) {
             PRINTLOG(LINKER, LOG_ERROR, "cannot get section size");
-            sec_rec->destroy(sec_rec);
 
             goto clean_secs_iter;
         }
 
         if(!sec_rec->get_int64(sec_rec, "alignment", &section_alignment)) {
             PRINTLOG(LINKER, LOG_ERROR, "cannot get section aligment");
-            sec_rec->destroy(sec_rec);
 
             goto clean_secs_iter;
         }
@@ -882,14 +861,12 @@ int8_t linker_build_module(linker_context_t* ctx, uint64_t module_id, boolean_t 
 
             if(!sec_rec->get_bytearray(sec_rec, "value", &tmp_section_size, &section_data)) {
                 PRINTLOG(LINKER, LOG_ERROR, "cannot get section data");
-                sec_rec->destroy(sec_rec);
 
                 goto clean_secs_iter;
             }
 
             if(!section_data) {
                 PRINTLOG(LINKER, LOG_ERROR, "section data is NULL");
-                sec_rec->destroy(sec_rec);
 
                 goto clean_secs_iter;
             }
@@ -897,7 +874,6 @@ int8_t linker_build_module(linker_context_t* ctx, uint64_t module_id, boolean_t 
             if(tmp_section_size != section_size) {
                 PRINTLOG(LINKER, LOG_ERROR, "section size mismatch");
                 memory_free(section_data);
-                sec_rec->destroy(sec_rec);
 
                 goto clean_secs_iter;
             }
@@ -917,7 +893,6 @@ int8_t linker_build_module(linker_context_t* ctx, uint64_t module_id, boolean_t 
             if(section_offset % section_alignment) {
                 PRINTLOG(LINKER, LOG_ERROR, "section offset alignment mismatch");
                 memory_free(section_data);
-                sec_rec->destroy(sec_rec);
 
                 goto clean_secs_iter;
             }
@@ -925,7 +900,6 @@ int8_t linker_build_module(linker_context_t* ctx, uint64_t module_id, boolean_t 
             if(section_offset != module->sections[section_type].size) {
                 PRINTLOG(LINKER, LOG_ERROR, "section offset mismatch");
                 memory_free(section_data);
-                sec_rec->destroy(sec_rec);
 
                 goto clean_secs_iter;
             }
@@ -933,7 +907,6 @@ int8_t linker_build_module(linker_context_t* ctx, uint64_t module_id, boolean_t 
             if(!buffer_append_bytes(module->sections[section_type].section_data, section_data, section_size)) {
                 PRINTLOG(LINKER, LOG_ERROR, "cannot append section data");
                 memory_free(section_data);
-                sec_rec->destroy(sec_rec);
 
                 goto clean_secs_iter;
             }
@@ -943,7 +916,6 @@ int8_t linker_build_module(linker_context_t* ctx, uint64_t module_id, boolean_t 
             if(section_end != section_offset + section_size) {
                 PRINTLOG(LINKER, LOG_ERROR, "section end mismatch");
                 memory_free(section_data);
-                sec_rec->destroy(sec_rec);
 
                 goto clean_secs_iter;
             }
@@ -955,7 +927,6 @@ int8_t linker_build_module(linker_context_t* ctx, uint64_t module_id, boolean_t 
 
         if(!sec_rec->get_string(sec_rec, "name", &section_name)) {
             PRINTLOG(LINKER, LOG_ERROR, "cannot get section name");
-            sec_rec->destroy(sec_rec);
 
             goto clean_secs_iter;
         }
@@ -967,14 +938,12 @@ int8_t linker_build_module(linker_context_t* ctx, uint64_t module_id, boolean_t 
 
         if(linker_build_symbols(ctx, module_id, section_id, section_type, section_offset) != 0) {
             PRINTLOG(LINKER, LOG_ERROR, "cannot build symbols for section id 0x%llx", section_id);
-            sec_rec->destroy(sec_rec);
 
             goto clean_secs_iter;
         }
 
         if(linker_build_relocations(ctx,  section_id, section_type, section_offset, module, recursive) != 0) {
             PRINTLOG(LINKER, LOG_ERROR, "cannot build relocations for section id 0x%llx", section_id);
-            sec_rec->destroy(sec_rec);
 
             goto clean_secs_iter;
         }
