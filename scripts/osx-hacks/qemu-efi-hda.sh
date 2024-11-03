3c18cbc56f6d19aeecb1ac8e74190410571ee801
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2024-11-03 08:58:31 +0000

    add: virtio serial toggle to qemu script
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- scripts/osx-hacks/qemu-efi-hda.sh
+++ scripts/osx-hacks/qemu-efi-hda.sh
@@ -50,11 +50,20 @@ RAMSIZE=4
 
 SERIALS=""
 
-TRACE_OPTS="guest_errors,trace:virtio_console_*"
-
 for i in `seq 0 $((NUMCPUS-1))`; do
   SERIALS="${SERIALS} -serial file:${BASEDIR}/tmp/qemu-serial${i}.log"
 done
+
+VIRTIO_SERIAL_ENABLED="yes"
+VIRTIO_SERIAL=""
+
+if [[ "${VIRTIO_SERIAL_ENABLED}x" == "yesx" ]]; then
+    VIRTIO_SERIAL="${VIRTIO_SERIAL} -device virtio-serial,id=vser0,packed=on,ioeventfd=on,max_ports=4,vectors=64"
+    VIRTIO_SERIAL="${VIRTIO_SERIAL} -device virtserialport,name=com.turnstoneos.clipboard.0,chardev=vdagent0"
+    VIRTIO_SERIAL="${VIRTIO_SERIAL} -chardev socket,id=vdagent0,port=4444,host=localhost,server=off,reconnect=5"
+fi
+
+TRACE_OPTS="guest_errors,trace:virtio_console_*"
 
 qemu-system-x86_64 \
   -nodefaults -no-user-config $PREVENTSHUTDOWN \
@@ -75,9 +84,7 @@ qemu-system-x86_64 \
   -device virtio-mouse,id=mouse \
   -device virtio-tablet,id=tablet \
   -device edu,id=edu,dma_mask=0xFFFFFFFFFFFFFFFF \
-  -device virtio-serial,id=vser0,packed=on,ioeventfd=on,max_ports=4,vectors=64 \
-  -device virtserialport,name=com.turnstoneos.clipboard.0,chardev=vdagent0 \
-  -chardev socket,id=vdagent0,port=4444,host=localhost,server=off,reconnect=5 \
+  $VIRTIO_SERIAL \
   $SERIALS \
   -debugcon file:${BASEDIR}/tmp/qemu-acpi-debug.log -global isa-debugcon.iobase=0x402 \
   -display sdl,gl=on,show-cursor=on 
