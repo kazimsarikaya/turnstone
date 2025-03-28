93b4503af26f805e2343b523a1ffaa98c0eb2868
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2025-03-28 03:43:16 +0000

    update: remove osx related code from scripts
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- scripts/osx-hacks/net-stop.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# This work is licensed under TURNSTONE OS Public License.
-# Please read and understand latest version of Licence.
-
-sudo port unload dnsmasq
-sudo ifconfig bridge0 down
-sudo ifconfig bridge0 destroy
-sudo kextunload  /Applications/Tunnelblick.app/Contents//Resources/tap-notarized.kext
--- scripts/qemu/qemu-efi-hda.sh
+++ scripts/qemu/qemu-efi-hda.sh
@@ -7,16 +7,25 @@ CURRENTDIR=`dirname $0`
 BASEDIR="${CURRENTDIR}/../../"
 OUTPUTDIR="${BASEDIR}/build"
 
-UEFIBIOSCODE=""
-UEFIBIOSVARS=""
-ACCEL="$([[ -c /dev/kvm ]] && echo kvm || echo tcg)"
-NETDEV=""
+ACCEL="kvm"
+UEFIBIOSCODESRC="/usr/share/OVMF/OVMF_CODE.fd"
+UEFIBIOSVARSSRC="/usr/share/OVMF/OVMF_VARS.fd"
+NETDEV="bridge,id=t0"
+
+if [ ! -c /dev/kvm ]; then
+    echo "KVM is not available. Please load the kvm module."
+    exit 1
+fi
 
 DEBUG=$1
 PREVENTSHUTDOWN=""
 
 if [[ "${DEBUG}x" == "debugx" ]]; then
-  ACCEL="tcg -s -S"
+  ACCEL="${ACCEL} -s -S"
+  PREVENTSHUTDOWN="--no-shutdown --no-reboot"
+fi
+
+if [[ "${DEBUG}x" == "preventx" ]]; then
   PREVENTSHUTDOWN="--no-shutdown --no-reboot"
 fi
 
@@ -24,32 +33,19 @@ if [[ "${DEBUG}x" == "pausedx" ]]; then
   ACCEL="${ACCEL} -S"
 fi
 
-if [ `uname -s` == "Linux" ]; then
-  UEFIBIOSCODE="/usr/share/OVMF/OVMF_CODE.fd"  
-  UEFIBIOSVARS="/usr/share/OVMF/OVMF_VARS.fd"
-  NETDEV="bridge,id=t0"
-  #NETDEV="tap,id=t0,ifname=tap0,script=no,downscript=no"
-fi
-
-if [ `uname -s` == "Darwin" ]; then
-  UEFIBIOSCODE="/opt/local/share/qemu/edk2-x86_64-code.fd"
-  UEFIBIOSVARS="/opt/local/share/qemu/edk2-i386-vars.fd"
-  NETDEV="socket,id=t0,udp=127.0.0.1:16384,localaddr=127.0.0.1:16385"
-fi
-
 if [ ! -f $CURRENTDIR/edk2-x86_64-code.fd ]; then
-  cp $UEFIBIOSCODE $CURRENTDIR/edk2-x86_64-code.fd
+  cp $UEFIBIOSCODESRC $OUTPUTDIR/edk2-x86_64-code.fd
 fi
 
 if [ ! -f $CURRENTDIR/edk2-i386-vars.fd ]; then
-  cp $UEFIBIOSVARS $CURRENTDIR/edk2-i386-vars.fd
+  cp $UEFIBIOSVARSSRC $OUTPUTDIR/edk2-i386-vars.fd
 fi
 
 if [ ! -f ${OUTPUTDIR}/qemu-nvme-cache ]; then
   dd if=/dev/zero of=${OUTPUTDIR}/qemu-nvme-cache bs=1 count=0 seek=$((1024*1024*1024)) >/dev/null 2>&1
 fi
 
-NUMCPUS=4 
+NUMCPUS=4
 RAMSIZE=8
 
 SERIALS=""
@@ -58,7 +54,7 @@ for i in `seq 0 $((NUMCPUS-1))`; do
   SERIALS="${SERIALS} -serial file:${BASEDIR}/tmp/qemu-serial${i}.log"
 done
 
-TRACE_OPTS="guest_errors,mmu,trace:amdvi*"
+TRACE_OPTS="guest_errors,mmu"
 
 # if trace_opts is not empty, then enable tracing (prefix with -d)
 
@@ -71,8 +67,8 @@ qemu-system-x86_64 \
   -M q35,kernel-irqchip=split -m ${RAMSIZE}g -smp cpus=${NUMCPUS} -name osdev-hda-efi-boot \
   -cpu host \
   -accel $ACCEL ${TRACE_OPTS} \
-  -drive if=pflash,readonly=on,format=raw,unit=0,file=${CURRENTDIR}/edk2-x86_64-code.fd \
-  -drive if=pflash,readonly=off,format=raw,unit=1,file=${CURRENTDIR}/edk2-i386-vars.fd \
+  -drive if=pflash,readonly=on,format=raw,unit=0,file=${OUTPUTDIR}/edk2-x86_64-code.fd \
+  -drive if=pflash,readonly=off,format=raw,unit=1,file=${OUTPUTDIR}/edk2-i386-vars.fd \
   -drive id=system,if=none,format=raw,file=${OUTPUTDIR}/qemu-hda,werror=report,rerror=report \
   -device ide-hd,drive=system,bootindex=1 \
   -drive id=cache,if=none,format=raw,file=${OUTPUTDIR}/qemu-nvme-cache,werror=report,rerror=report \
