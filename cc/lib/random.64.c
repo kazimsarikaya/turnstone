641b8b7661424c655fea72200b7a9a2ced635027
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2025-03-31 19:06:53 +0000

    add: hardware assisted random
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- cc/lib/random.64.c
+++ cc/lib/random.64.c
@@ -8,27 +8,27 @@
 
 #include <random.h>
 #include <xxhash.h>
+#include <cpu.h>
 
 MODULE("turnstone.lib.random");
 
-uint64_t random_xoroshiro_seed = 0;
-uint64_t random_xoroshiro_state[4] = {};
-
-uint64_t random_xoroshiro_state_next(void);
-uint64_t random_xoroshiro_next(void);
+static uint64_t random_xoroshiro_seed = 0;
+static uint64_t random_xoroshiro_state[4] = {};
+static boolean_t rdrand_supported = false;
+static boolean_t rdrand_initialized = false;
 
 static inline uint64_t random_xoroshiro_rotl(const uint64_t x, int k) {
     return (x << k) | (x >> (64 - k));
 }
 
-uint64_t random_xoroshiro_state_next(void) {
+static uint64_t random_xoroshiro_state_next(void) {
     uint64_t z = (random_xoroshiro_seed += 0x9e3779b97f4a7c15ULL);
     z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9ULL;
     z = (z ^ (z >> 27)) * 0x94d049bb133111ebULL;
     return z ^ (z >> 31);
 }
 
-uint64_t random_xoroshiro_next(void) {
+static uint64_t random_xoroshiro_next(void) {
     const uint64_t result = random_xoroshiro_rotl(random_xoroshiro_state[0] + random_xoroshiro_state[3], 23) + random_xoroshiro_state[0];
 
     const uint64_t t = random_xoroshiro_state[1] << 17;
@@ -55,9 +55,44 @@ void srand(uint64_t seed) {
 }
 
 uint32_t rand(void) {
-    return random_xoroshiro_next() >> 32;
+    if(!rdrand_initialized) {
+        rdrand_supported = cpu_check_rdrand() == 0;
+        rdrand_initialized = true;
+    }
+
+    if(!rdrand_supported) {
+        return random_xoroshiro_next() >> 32;
+    }
+
+    boolean_t valid = false;
+    uint32_t result = 0;
+
+    while(!valid) {
+        // if cf is set, then the value is valid
+        asm volatile ("rdrand %0; setc %1" : "=r" (result), "=qm" (valid));
+    }
+
+    return result;
 }
 
 uint64_t rand64(void) {
-    return random_xoroshiro_next();
+    if(!rdrand_initialized) {
+        rdrand_supported = cpu_check_rdrand() == 0;
+        rdrand_initialized = true;
+    }
+
+    if(!rdrand_supported) {
+        return random_xoroshiro_next();
+    }
+
+    boolean_t valid = false;
+    uint64_t result = 0;
+
+    while(!valid) {
+        // if cf is set, then the value is valid
+        asm volatile ("rdrand %0; setc %1" : "=r" (result), "=qm" (valid));
+
+    }
+
+    return result;
 }
