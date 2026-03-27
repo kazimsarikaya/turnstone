a95dab9823ca86dc23e3d42e45da9e462f338daa
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2026-03-27 08:23:10 +0000

    add: restore pci and vga
    also some refactoring and bug fixes.
    
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- cc/cpu/smp.64.c
+++ cc/cpu/smp.64.c
@@ -23,13 +23,11 @@
 #include <cpu/cpu_state.h>
 #include <hypervisor/hypervisor.h>
 #include <systeminfo.h>
+#include <pci.h>
+#include <driver/video.h>
+#include <graphics/screen.h>
 
 MODULE("turnstone.kernel.cpu.smp");
-
-void video_text_print(const char_t* str);
-
-int8_t  smp_init_cpu(uint8_t cpu_id);
-int32_t smp_ap_boot(uint8_t cpu_id);
 
 const uint8_t trampoline_code[] = {
     0xea, 0x38, 0x80, 0x00, 0x00, // 8000: jmp $0x0:$0x8038
@@ -106,7 +104,7 @@ const uint8_t trampoline_code[] = {
 };
 
 
-int8_t smp_init_cpu(uint8_t cpu_id) {
+static int8_t smp_init_cpu(uint8_t cpu_id) {
     PRINTLOG(APIC, LOG_INFO, "SMP: Initialising CPU %d", cpu_id);
 
     apic_send_init(cpu_id);
@@ -118,102 +116,7 @@ int8_t smp_init_cpu(uint8_t cpu_id) {
     return 0;
 }
 
-
-int8_t smp_init(void) {
-    PRINTLOG(APIC, LOG_INFO, "SMP Initialisation");
-
-    uint32_t local_apic_id = cpu_state->local_apic_id;
-
-    PRINTLOG(APIC, LOG_INFO, "SMP: Bootstrap APIC ID: 0x%x", local_apic_id);
-    uint8_t ap_cpu_count = apic_get_ap_count();
-
-    if(ap_cpu_count == 0) {
-        PRINTLOG(APIC, LOG_INFO, "SMP: No APs found");
-        PRINTLOG(APIC, LOG_INFO, "SMP: No need to initialise SMP");
-
-        return 0;
-    }
-
-    acpi_sdt_header_t* madt = acpi_get_table(ACPI_CONTEXT->xrsdp_desc, "APIC");
-
-    if (madt == NULL) {
-        PRINTLOG(APIC, LOG_ERROR, "SMP: MADT not found");
-        return -1;
-    }
-
-    memory_paging_add_page(0x8000, 0x8000, MEMORY_PAGING_PAGE_TYPE_4K);
-
-    uint8_t * trampoline = (uint8_t*)0x8000;
-
-    memory_paging_add_page(0x9000, 0x9000, MEMORY_PAGING_PAGE_TYPE_4K);
-
-    smp_data_t* smp_data = (smp_data_t*)0x9000;
-
-    memory_memcopy(trampoline_code, trampoline, sizeof(trampoline_code));
-
-    uint32_t* trampoline_call_addr = (uint32_t*)(void*)(trampoline + 0x110);
-
-    *trampoline_call_addr = (uint32_t)((uint64_t)smp_ap_boot);
-
-    list_t* apic_entries = acpi_get_apic_table_entries(madt);
-
-    frame_t* stack_frames;
-    uint64_t stack_frames_cnt = 16 * (ap_cpu_count + 1);
-    uint64_t stack_size       = 16 * FRAME_SIZE;
-
-    if(frame_get_allocator()->allocate_frame_by_count(frame_get_allocator(), stack_frames_cnt,
-                                                      FRAME_ALLOCATION_TYPE_USED | FRAME_ALLOCATION_TYPE_BLOCK,
-                                                      &stack_frames, NULL) != 0) {
-        PRINTLOG(APIC, LOG_ERROR, "SMP: Failed to allocate stack frames");
-        return -1;
-    }
-
-    uint64_t stack_frames_va = MEMORY_PAGING_GET_VA_FOR_RESERVED_FA(stack_frames->frame_address);
-
-    if(memory_paging_add_va_for_frame(stack_frames_va, stack_frames, MEMORY_PAGING_PAGE_TYPE_4K) != 0) {
-        PRINTLOG(APIC, LOG_ERROR, "SMP: Failed to map stack frames");
-        return -1;
-    }
-
-    memory_memclean((void*)stack_frames_va, stack_frames_cnt * FRAME_SIZE);
-
-    PRINTLOG(APIC, LOG_INFO, "SMP: Stack frames allocated at fa 0x%llx with count 0x%llx", stack_frames->frame_address, stack_frames->frame_count);
-    PRINTLOG(APIC, LOG_INFO, "SMP: Stack frames mapped at va 0x%llx with size 0x%llx", stack_frames_va, stack_size);
-
-    uint64_t ap_gs_size = 4 * FRAME_SIZE;
-
-    uint64_t ap_gs_va = SYSTEM_INFO->gs_page_address_base;
-
-    smp_data->stack_base        = stack_frames_va;
-    smp_data->stack_size        = stack_size;
-    smp_data->cr0               = cpu_read_cr0();
-    smp_data->cr3               = MEMORY_PAGING_GET_FA_FOR_RESERVED_VA(memory_paging_get_table()->page_table);
-    smp_data->cr4               = cpu_read_cr4();
-    smp_data->lock              = lock_create_with_heap(memory_get_default_heap());
-    smp_data->running_cpu_count = 1; // we are already running on one cpu, so start with 1.
-    smp_data->gs_base           = ap_gs_va;
-    smp_data->gs_base_size      = ap_gs_size;
-
-    for(uint64_t i = 0; i < list_size(apic_entries); i++) {
-        const acpi_table_madt_entry_t* e = list_get_data_at_position(apic_entries, i);
-
-        if(e->info.type == ACPI_MADT_ENTRY_TYPE_PROCESSOR_LOCAL_APIC) {
-            uint8_t apic_id = e->processor_local_apic.apic_id;
-
-            PRINTLOG(APIC, LOG_INFO, "SMP: Found APIC ID: %d local apic? %i", apic_id, apic_id == local_apic_id);
-
-            if(apic_id != local_apic_id) {
-                smp_init_cpu(apic_id);
-            }
-        }
-    }
-
-    return 0;
-}
-
-extern boolean_t GRAPHICS_MODE;
-
-int32_t smp_ap_boot(uint8_t cpu_id) {
+static int32_t smp_ap_boot(uint8_t cpu_id) {
     cpu_cli();
 
     cpu_enable_sse();
@@ -228,8 +131,8 @@ int32_t smp_ap_boot(uint8_t cpu_id) {
     cpu_write_msr(CPU_MSR_IA32_GS_BASE, gs_base);
 
     if(smp_data->is_for_wakeup && cpu_state->local_apic_id == 0) { // only bsp should set area.
-        GRAPHICS_MODE               = false; // we need to set this after init devices.
-        smp_data->running_cpu_count = 0; // we will set our self soon.
+        video_set_graphics_mode(false);
+        smp_data->running_cpu_count = 1; // we are already running on one cpu, so start with 1. APs will increment this when they boot.
     }
 
     if(smp_data->is_for_wakeup &&
@@ -321,12 +224,16 @@ int32_t smp_ap_boot(uint8_t cpu_id) {
         PRINTLOG(KERNEL, LOG_INFO, "AP %i woke up task with id 0x%llx on cpu %lli", cpu_id, wakeup_task->task_id, wakeup_task->cpu_id);
     }
 
-    lock_acquire(smp_data->lock);
-    smp_data->running_cpu_count++;
-    lock_release(smp_data->lock);
-
-
     if(smp_data->is_for_wakeup && cpu_state->local_apic_id == 0) { // only bsp should set area.
+        if(pci_restore_registers() != 0) {
+            PRINTLOG(KERNEL, LOG_ERROR, "cannot restore pci registers after wakeup");
+
+            cpu_hlt();
+        }
+
+        video_display_reinit();
+        video_set_graphics_mode(true);
+
 
         // wake up other cpus.
         PRINTLOG(KERNEL, LOG_INFO, "AP %i waking up other cpus.", cpu_id);
@@ -337,11 +244,7 @@ int32_t smp_ap_boot(uint8_t cpu_id) {
 
             PRINTLOG(KERNEL, LOG_INFO, "AP %i sending wakeup signal to cpu %lli.", cpu_id, i);
 
-            apic_send_init(i);
-
-            apic_send_sipi(i, 0x08);
-
-            apic_send_sipi(i, 0x08);
+            smp_init_cpu(i);
 
             PRINTLOG(KERNEL, LOG_INFO, "AP %i sent wakeup signal to cpu %lli.", cpu_id, i);
         }
@@ -361,10 +264,22 @@ int32_t smp_ap_boot(uint8_t cpu_id) {
         }
 
         smp_data->is_for_wakeup = false;
+
+        PRINTLOG(KERNEL, LOG_INFO, "AP %i wakeup completed. All %lli cpus are running.", cpu_id, smp_data->running_cpu_count);
+
+        task_exit(0);
+
+        PRINTLOG(KERNEL, LOG_ERROR, "we should never reach here after task exit");
+
+        return 0;
     }
 
 
     if(smp_data->is_for_wakeup) {
+        lock_acquire(smp_data->lock);
+        smp_data->running_cpu_count++;
+        lock_release(smp_data->lock);
+
         PRINTLOG(KERNEL, LOG_INFO, "AP %i wakeup completed.", cpu_id);
 
         task_exit(0);
@@ -372,6 +287,10 @@ int32_t smp_ap_boot(uint8_t cpu_id) {
         PRINTLOG(KERNEL, LOG_ERROR, "we should never reach here after task exit");
 
         return 0;
+    } else {
+        lock_acquire(smp_data->lock);
+        smp_data->running_cpu_count++;
+        lock_release(smp_data->lock);
     }
 
 
@@ -457,6 +376,98 @@ int32_t smp_ap_boot(uint8_t cpu_id) {
 
     while(true) { // never reach here
         cpu_hlt();
+    }
+
+    return 0;
+}
+
+int8_t smp_init(void) {
+    PRINTLOG(APIC, LOG_INFO, "SMP Initialisation");
+
+    uint32_t local_apic_id = cpu_state->local_apic_id;
+
+    PRINTLOG(APIC, LOG_INFO, "SMP: Bootstrap APIC ID: 0x%x", local_apic_id);
+    uint8_t ap_cpu_count = apic_get_ap_count();
+
+    if(ap_cpu_count == 0) {
+        PRINTLOG(APIC, LOG_INFO, "SMP: No APs found");
+        PRINTLOG(APIC, LOG_INFO, "SMP: No need to initialise SMP");
+
+        return 0;
+    }
+
+    acpi_sdt_header_t* madt = acpi_get_table(ACPI_CONTEXT->xrsdp_desc, "APIC");
+
+    if (madt == NULL) {
+        PRINTLOG(APIC, LOG_ERROR, "SMP: MADT not found");
+        return -1;
+    }
+
+    memory_paging_add_page(0x8000, 0x8000, MEMORY_PAGING_PAGE_TYPE_4K);
+
+    uint8_t * trampoline = (uint8_t*)0x8000;
+
+    memory_paging_add_page(0x9000, 0x9000, MEMORY_PAGING_PAGE_TYPE_4K);
+
+    smp_data_t* smp_data = (smp_data_t*)0x9000;
+
+    memory_memcopy(trampoline_code, trampoline, sizeof(trampoline_code));
+
+    uint32_t* trampoline_call_addr = (uint32_t*)(void*)(trampoline + 0x110);
+
+    *trampoline_call_addr = (uint32_t)((uint64_t)smp_ap_boot);
+
+    list_t* apic_entries = acpi_get_apic_table_entries(madt);
+
+    frame_t* stack_frames;
+    uint64_t stack_frames_cnt = 16 * (ap_cpu_count + 1);
+    uint64_t stack_size       = 16 * FRAME_SIZE;
+
+    if(frame_get_allocator()->allocate_frame_by_count(frame_get_allocator(), stack_frames_cnt,
+                                                      FRAME_ALLOCATION_TYPE_USED | FRAME_ALLOCATION_TYPE_BLOCK,
+                                                      &stack_frames, NULL) != 0) {
+        PRINTLOG(APIC, LOG_ERROR, "SMP: Failed to allocate stack frames");
+        return -1;
+    }
+
+    uint64_t stack_frames_va = MEMORY_PAGING_GET_VA_FOR_RESERVED_FA(stack_frames->frame_address);
+
+    if(memory_paging_add_va_for_frame(stack_frames_va, stack_frames, MEMORY_PAGING_PAGE_TYPE_4K) != 0) {
+        PRINTLOG(APIC, LOG_ERROR, "SMP: Failed to map stack frames");
+        return -1;
+    }
+
+    memory_memclean((void*)stack_frames_va, stack_frames_cnt * FRAME_SIZE);
+
+    PRINTLOG(APIC, LOG_INFO, "SMP: Stack frames allocated at fa 0x%llx with count 0x%llx", stack_frames->frame_address, stack_frames->frame_count);
+    PRINTLOG(APIC, LOG_INFO, "SMP: Stack frames mapped at va 0x%llx with size 0x%llx", stack_frames_va, stack_size);
+
+    uint64_t ap_gs_size = 4 * FRAME_SIZE;
+
+    uint64_t ap_gs_va = SYSTEM_INFO->gs_page_address_base;
+
+    smp_data->stack_base        = stack_frames_va;
+    smp_data->stack_size        = stack_size;
+    smp_data->cr0               = cpu_read_cr0();
+    smp_data->cr3               = MEMORY_PAGING_GET_FA_FOR_RESERVED_VA(memory_paging_get_table()->page_table);
+    smp_data->cr4               = cpu_read_cr4();
+    smp_data->lock              = lock_create_with_heap(memory_get_default_heap());
+    smp_data->running_cpu_count = 1; // we are already running on one cpu, so start with 1.
+    smp_data->gs_base           = ap_gs_va;
+    smp_data->gs_base_size      = ap_gs_size;
+
+    for(uint64_t i = 0; i < list_size(apic_entries); i++) {
+        const acpi_table_madt_entry_t* e = list_get_data_at_position(apic_entries, i);
+
+        if(e->info.type == ACPI_MADT_ENTRY_TYPE_PROCESSOR_LOCAL_APIC) {
+            uint8_t apic_id = e->processor_local_apic.apic_id;
+
+            PRINTLOG(APIC, LOG_INFO, "SMP: Found APIC ID: %d local apic? %i", apic_id, apic_id == local_apic_id);
+
+            if(apic_id != local_apic_id) {
+                smp_init_cpu(apic_id);
+            }
+        }
     }
 
     return 0;
