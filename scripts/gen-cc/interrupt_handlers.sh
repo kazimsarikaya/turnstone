1212c7bba0544888e7efd0d96c5462ff807247c6
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2026-03-28 10:30:22 +0000

    fix: remove deps
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- scripts/gen-cc/interrupt_handlers.sh
+++ scripts/gen-cc/interrupt_handlers.sh
@@ -283,7 +283,8 @@ static void interrupt_dummy_handler(void) {
 }
 
 void interrupt_inject_dummy_interrupt_handler(uint8_t int_no) {
-    descriptor_register_t int_desc = descriptor_get_idt_register();
+    descriptor_register_t int_desc;
+    asm volatile ("sidt %0" : "=m" (int_desc));
     descriptor_idt_t* idt = (descriptor_idt_t*)int_desc.base;
     uint64_t fa = (uint64_t)interrupt_dummy_handler;
     idt[int_no].ist = 0;
