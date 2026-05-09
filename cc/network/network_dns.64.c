0c91e56e9dd419a4462829a22ec83aad08d455bf
Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>
2026-05-09 08:57:51 +0000

    update: improve dns
    now we have new dns record types.
    
    Signed-off-by: Kazım SARIKAYA <kazimsarikaya@sanaldiyar.com>

--- cc/network/network_dns.64.c
+++ cc/network/network_dns.64.c
@@ -12,14 +12,11 @@
 #include <memory.h>
 #include <logging.h>
 #include <random.h>
+#include <strings.h>
 
 MODULE("turnstone.lib.network");
 
 extern memory_heap_t* network_packet_heap;
-
-typedef enum network_dns_class_t : uint16_t {
-    NETWORK_DNS_CLASS_IN = 1, // Internet
-} network_dns_class_t;
 
 typedef struct network_dns_query_header_t {
     uint16_t id;
@@ -30,16 +27,100 @@ typedef struct network_dns_query_header_t {
     uint16_t additional_count;
 } __attribute((packed)) network_dns_query_header_t;
 
-#define NETWORK_DNS_MAX_IPV4_ADDRESSES 16
+#define NETWORK_DNS_MAX_RECORDS 16
+
+typedef struct network_dns_mx_record_t {
+    uint16_t preference;
+    char_t*  exchange;
+} network_dns_mx_record_t;
+
+typedef struct network_dns_soa_record_t {
+    char_t*  mname;
+    char_t*  rname;
+    uint32_t serial;
+    uint32_t refresh;
+    uint32_t retry;
+    uint32_t expire;
+    uint32_t minimum;
+} network_dns_soa_record_t;
 
 struct network_dns_query_result_t {
     network_dns_record_type_t record_type;
     uint32_t                  record_count;
     union {
-        network_ipv4_address_t ipv4_addresses[NETWORK_DNS_MAX_IPV4_ADDRESSES];
-        // other record types can be added here
+        network_ipv4_address_t   ipv4_addresses[NETWORK_DNS_MAX_RECORDS];
+        char_t*                  names[NETWORK_DNS_MAX_RECORDS]; // For NS, CNAME, PTR, TXT
+        network_dns_mx_record_t  mx_records[NETWORK_DNS_MAX_RECORDS];
+        network_dns_soa_record_t soa_records[NETWORK_DNS_MAX_RECORDS];
     };
 };
+
+static int32_t parse_dns_name(const uint8_t* packet, uint32_t packet_len, const uint8_t* name_ptr, char_t* out_name, uint32_t max_len) {
+    uint32_t out_pos       = 0;
+    uint32_t bytes_read    = 0;
+    boolean_t jumped       = false;
+    const uint8_t* current = name_ptr;
+    uint32_t jumps         = 0;
+
+    while (true) {
+        if (current < packet || current >= packet + packet_len) {
+            return -1;
+        }
+        if (*current == 0) {
+            break;
+        }
+
+        if (jumps > 100) {
+            return -1; // Prevent infinite loops
+        }
+        if ((*current & 0xC0) == 0xC0) {
+            if (!jumped) {
+                bytes_read += 2;
+            }
+            if (current + 1 >= packet + packet_len) {
+                return -1;
+            }
+            uint16_t offset = (BYTE_SWAP16(*(uint16_t*)(void*)current)) & 0x3FFF;
+            if (offset >= packet_len) {
+                return -1;
+            }
+            current = packet + offset;
+            jumped  = true;
+            jumps++;
+        } else {
+            uint8_t label_len = *current;
+            if (!jumped) {
+                bytes_read += label_len + 1;
+            }
+            current++;
+            if (current + label_len > packet + packet_len) {
+                return -1;
+            }
+            if (out_name) {
+                if (out_pos + label_len + 1 > max_len) {
+                    return -1;
+                }
+                memory_memcopy(current, out_name + out_pos, label_len);
+                out_pos            += label_len;
+                out_name[out_pos++] = '.';
+            }
+            current += label_len;
+        }
+    }
+    if (!jumped) {
+        bytes_read++;
+    }
+    if (out_name) {
+        if (out_pos > 0) {
+            out_name[out_pos - 1] = '\0'; // Remove trailing dot
+        } else {
+            if (max_len > 0) {
+                out_name[0] = '\0';
+            }
+        }
+    }
+    return bytes_read;
+}
 
 network_dns_query_result_t* network_dns_query(network_ipv4_address_t    dns_server,
                                               const char_t*             domain_name,
@@ -179,19 +260,11 @@ network_dns_query_result_t* network_dns_query(network_ipv4_address_t    dns_serv
     uint8_t* current_ptr = dns_response_packet + sizeof(network_dns_query_header_t);
 
     // Skip question section
-    boolean_t is_pointer = false;
-    while(*current_ptr != 0) {
-        if((*current_ptr & 0xC0) == 0xC0) { // Pointer
-            current_ptr += 2;
-            is_pointer   = true;
-            break;
-        } else {
-            current_ptr += *current_ptr + 1;
-        }
+    int32_t qname_len = parse_dns_name(dns_response_packet, recv_len, current_ptr, NULL, 0);
+    if (qname_len < 0) {
+        goto cleanup;
     }
-    if(!is_pointer) {
-        current_ptr++; // Skip the 0 byte at the end of the name
-    }
+    current_ptr += qname_len;
 
     current_ptr += sizeof(uint16_t) * 2; // Skip QTYPE and QCLASS
 
@@ -201,23 +274,18 @@ network_dns_query_result_t* network_dns_query(network_ipv4_address_t    dns_serv
         goto cleanup;
     }
 
+    result->record_type  = record_type;
+    result->record_count = 0;
+
     PRINTLOG(NETWORK, LOG_TRACE, "Parsing DNS answer section with %u answers", answer_count);
 
     for(uint16_t i = 0; i < answer_count; ++i) {
         // Skip name
-        is_pointer = false;
-        while(*current_ptr != 0) {
-            if((*current_ptr & 0xC0) == 0xC0) { // Pointer
-                current_ptr += 2;
-                is_pointer   = true;
-                break;
-            } else {
-                current_ptr += *current_ptr + 1;
-            }
+        int32_t name_len = parse_dns_name(dns_response_packet, recv_len, current_ptr, NULL, 0);
+        if (name_len < 0) {
+            goto cleanup;
         }
-        if(!is_pointer) {
-            current_ptr++; // Skip the 0 byte at the end of the name
-        }
+        current_ptr += name_len;
 
         uint16_t type = BYTE_SWAP16(*(uint16_t*)(void*)current_ptr);
         current_ptr += sizeof(uint16_t);
@@ -232,14 +300,63 @@ network_dns_query_result_t* network_dns_query(network_ipv4_address_t    dns_serv
 
         UNUSED(ttl);
 
-        if(type == NETWORK_DNS_RECORD_TYPE_A && class == NETWORK_DNS_CLASS_IN) { // A record, IN class
-            if(rdlength == sizeof(network_ipv4_address_t)) {
-                if(result->record_count < NETWORK_DNS_MAX_IPV4_ADDRESSES) {
-                    memory_memcopy(current_ptr, &result->ipv4_addresses[result->record_count], sizeof(network_ipv4_address_t));
-                    result->record_count++;
-                } else {
-                    PRINTLOG(NETWORK, LOG_WARNING, "Too many IPv4 addresses in DNS response, skipping some.");
+        if(type == record_type && class == NETWORK_DNS_CLASS_IN) {
+            if(result->record_count < NETWORK_DNS_MAX_RECORDS) {
+                if (type == NETWORK_DNS_RECORD_TYPE_A) {
+                    if(rdlength == sizeof(network_ipv4_address_t)) {
+                        memory_memcopy(current_ptr, &result->ipv4_addresses[result->record_count], sizeof(network_ipv4_address_t));
+                        result->record_count++;
+                    }
+                } else if (type == NETWORK_DNS_RECORD_TYPE_NS || type == NETWORK_DNS_RECORD_TYPE_CNAME || type == NETWORK_DNS_RECORD_TYPE_PTR) {
+                    char_t name_buf[256];
+                    if (parse_dns_name(dns_response_packet, recv_len, current_ptr, name_buf, sizeof(name_buf)) > 0) {
+                        result->names[result->record_count] = strdup_at_heap(network_packet_heap, name_buf);
+                        result->record_count++;
+                    }
+                } else if (type == NETWORK_DNS_RECORD_TYPE_TXT) {
+                    if (rdlength > 0) {
+                        uint8_t txt_len = *current_ptr;
+                        if (txt_len <= rdlength - 1) {
+                            char_t* txt = memory_malloc_ext(network_packet_heap, txt_len + 1, 0);
+                            if (txt) {
+                                memory_memcopy(current_ptr + 1, txt, txt_len);
+                                txt[txt_len]                        = '\0';
+                                result->names[result->record_count] = txt;
+                                result->record_count++;
+                            }
+                        }
+                    }
+                } else if (type == NETWORK_DNS_RECORD_TYPE_MX) {
+                    if (rdlength >= 2) {
+                        uint16_t pref = BYTE_SWAP16(*(uint16_t*)(void*)current_ptr);
+                        char_t name_buf[256];
+                        if (parse_dns_name(dns_response_packet, recv_len, current_ptr + 2, name_buf, sizeof(name_buf)) > 0) {
+                            result->mx_records[result->record_count].preference = pref;
+                            result->mx_records[result->record_count].exchange   = strdup_at_heap(network_packet_heap, name_buf);
+                            result->record_count++;
+                        }
+                    }
+                } else if (type == NETWORK_DNS_RECORD_TYPE_SOA) {
+                    char_t mname_buf[256];
+                    char_t rname_buf[256];
+                    int32_t mname_len = parse_dns_name(dns_response_packet, recv_len, current_ptr, mname_buf, sizeof(mname_buf));
+                    if (mname_len > 0) {
+                        int32_t rname_len = parse_dns_name(dns_response_packet, recv_len, current_ptr + mname_len, rname_buf, sizeof(rname_buf));
+                        if (rname_len > 0 && rdlength >= mname_len + rname_len + 20) {
+                            const uint8_t* nums = current_ptr + mname_len + rname_len;
+                            result->soa_records[result->record_count].mname   = strdup_at_heap(network_packet_heap, mname_buf);
+                            result->soa_records[result->record_count].rname   = strdup_at_heap(network_packet_heap, rname_buf);
+                            result->soa_records[result->record_count].serial  = BYTE_SWAP32(*(uint32_t*)(void*)nums);
+                            result->soa_records[result->record_count].refresh = BYTE_SWAP32(*(uint32_t*)(void*)(nums + 4));
+                            result->soa_records[result->record_count].retry   = BYTE_SWAP32(*(uint32_t*)(void*)(nums + 8));
+                            result->soa_records[result->record_count].expire  = BYTE_SWAP32(*(uint32_t*)(void*)(nums + 12));
+                            result->soa_records[result->record_count].minimum = BYTE_SWAP32(*(uint32_t*)(void*)(nums + 16));
+                            result->record_count++;
+                        }
+                    }
                 }
+            } else {
+                PRINTLOG(NETWORK, LOG_WARNING, "Too many records in DNS response, skipping some.");
             }
         } else {
             PRINTLOG(NETWORK, LOG_WARNING, "Unsupported DNS record type %u or class %u, skipping.", type, class);
@@ -251,7 +368,7 @@ network_dns_query_result_t* network_dns_query(network_ipv4_address_t    dns_serv
 
 cleanup:
     if(err < 0 && result) {
-        memory_free_ext(network_packet_heap, result);
+        network_dns_query_result_free(result);
         result = NULL;
     }
 
@@ -264,22 +381,101 @@ cleanup:
 
 void network_dns_query_result_free(network_dns_query_result_t* result) {
     if(result) {
+        if (result->record_type == NETWORK_DNS_RECORD_TYPE_NS ||
+            result->record_type == NETWORK_DNS_RECORD_TYPE_CNAME ||
+            result->record_type == NETWORK_DNS_RECORD_TYPE_PTR ||
+            result->record_type == NETWORK_DNS_RECORD_TYPE_TXT) {
+            for (uint32_t i = 0; i < result->record_count; i++) {
+                if (result->names[i]) {
+                    memory_free_ext(network_packet_heap, result->names[i]);
+                }
+            }
+        } else if (result->record_type == NETWORK_DNS_RECORD_TYPE_MX) {
+            for (uint32_t i = 0; i < result->record_count; i++) {
+                if (result->mx_records[i].exchange) {
+                    memory_free_ext(network_packet_heap, result->mx_records[i].exchange);
+                }
+            }
+        } else if (result->record_type == NETWORK_DNS_RECORD_TYPE_SOA) {
+            for (uint32_t i = 0; i < result->record_count; i++) {
+                if (result->soa_records[i].mname) {
+                    memory_free_ext(network_packet_heap, result->soa_records[i].mname);
+                }
+                if (result->soa_records[i].rname) {
+                    memory_free_ext(network_packet_heap, result->soa_records[i].rname);
+                }
+            }
+        }
         memory_free_ext(network_packet_heap, result);
     }
 }
 
+uint32_t network_dns_query_result_get_record_count(network_dns_query_result_t* result) {
+    if(result == NULL) {
+        return 0;
+    }
+
+    return result->record_count;
+}
+
 network_ipv4_address_t network_dns_query_result_get_ipv4_address(network_dns_query_result_t* result, uint32_t index) {
-    if(result == NULL || index >= result->record_count) {
+    if(result == NULL || index >= result->record_count || result->record_type != NETWORK_DNS_RECORD_TYPE_A) {
         return (network_ipv4_address_t){0};
     }
 
     return result->ipv4_addresses[index];
 }
 
-uint32_t network_dns_query_result_get_ipv4_address_count(network_dns_query_result_t* result) {
-    if(result == NULL) {
-        return 0;
+const char_t* network_dns_query_result_get_name(network_dns_query_result_t* result, uint32_t index) {
+    if(result == NULL || index >= result->record_count) {
+        return NULL;
     }
+    if (result->record_type == NETWORK_DNS_RECORD_TYPE_NS ||
+        result->record_type == NETWORK_DNS_RECORD_TYPE_CNAME ||
+        result->record_type == NETWORK_DNS_RECORD_TYPE_PTR ||
+        result->record_type == NETWORK_DNS_RECORD_TYPE_TXT) {
+        return result->names[index];
+    }
+    return NULL;
+}
 
-    return result->record_count;
+boolean_t network_dns_query_result_get_mx_internal(network_dns_query_result_t* result, network_dns_query_mx_result_t mx_result) {
+    if(result == NULL || mx_result.index >= result->record_count || result->record_type != NETWORK_DNS_RECORD_TYPE_MX) {
+        return false;
+    }
+    if (mx_result.preference) {
+        *mx_result.preference = result->mx_records[mx_result.index].preference;
+    }
+    if (mx_result.exchange) {
+        *mx_result.exchange = result->mx_records[mx_result.index].exchange;
+    }
+    return true;
+}
+
+boolean_t network_dns_query_result_get_soa_internal(network_dns_query_result_t* result, network_dns_query_soa_result_t soa_result) {
+    if(result == NULL || soa_result.index >= result->record_count || result->record_type != NETWORK_DNS_RECORD_TYPE_SOA) {
+        return false;
+    }
+    if (soa_result.mname) {
+        *soa_result.mname = result->soa_records[soa_result.index].mname;
+    }
+    if (soa_result.rname) {
+        *soa_result.rname = result->soa_records[soa_result.index].rname;
+    }
+    if (soa_result.serial) {
+        *soa_result.serial = result->soa_records[soa_result.index].serial;
+    }
+    if (soa_result.refresh) {
+        *soa_result.refresh = result->soa_records[soa_result.index].refresh;
+    }
+    if (soa_result.retry) {
+        *soa_result.retry = result->soa_records[soa_result.index].retry;
+    }
+    if (soa_result.expire) {
+        *soa_result.expire = result->soa_records[soa_result.index].expire;
+    }
+    if (soa_result.minimum) {
+        *soa_result.minimum = result->soa_records[soa_result.index].minimum;
+    }
+    return true;
 }
--- includes/network/network_dns.h
+++ includes/network/network_dns.h
@@ -18,6 +18,10 @@
 extern "C" {
 #endif
 
+typedef enum network_dns_class_t : uint16_t {
+    NETWORK_DNS_CLASS_IN = 1, // Internet
+} network_dns_class_t;
+
 typedef enum network_dns_record_type_t : uint16_t {
     NETWORK_DNS_RECORD_TYPE_A     = 1,
     NETWORK_DNS_RECORD_TYPE_NS    = 2,
@@ -37,8 +41,46 @@ network_dns_query_result_t* network_dns_query(network_ipv4_address_t    dns_serv
 
 void network_dns_query_result_free(network_dns_query_result_t* result);
 
+uint32_t               network_dns_query_result_get_record_count(network_dns_query_result_t* result);
 network_ipv4_address_t network_dns_query_result_get_ipv4_address(network_dns_query_result_t* result, uint32_t index);
-uint32_t               network_dns_query_result_get_ipv4_address_count(network_dns_query_result_t* result);
+const char_t*          network_dns_query_result_get_name(network_dns_query_result_t* result, uint32_t index);
+
+typedef struct network_dns_query_mx_result_t {
+    const char_t** exchange;
+    uint16_t*      preference;
+    uint32_t       index;
+} network_dns_query_mx_result_t;
+
+boolean_t network_dns_query_result_get_mx_internal(network_dns_query_result_t* result, network_dns_query_mx_result_t mx_result);
+
+#define network_dns_query_result_get_mx(r, ex, ...) ({ \
+        _Pragma("GCC diagnostic push") \
+        _Pragma("GCC diagnostic ignored \"-Woverride-init\"") \
+        int8_t __rc = network_dns_query_result_get_mx_internal(r,(network_dns_query_mx_result_t){ .index = 0, .preference = NULL, .exchange = (ex) ,##__VA_ARGS__}); \
+        _Pragma("GCC diagnostic pop") \
+        __rc; \
+        })
+
+typedef struct network_dns_query_soa_result_t {
+    const char_t** rname;
+    uint32_t*      serial;
+    const char_t** mname;
+    uint32_t*      refresh;
+    uint32_t*      retry;
+    uint32_t*      expire;
+    uint32_t*      minimum;
+    uint32_t       index;
+} network_dns_query_soa_result_t;
+
+boolean_t network_dns_query_result_get_soa_internal(network_dns_query_result_t* result, network_dns_query_soa_result_t soa_result);
+
+#define network_dns_query_result_get_soa(r, rn, ...) ({ \
+        _Pragma("GCC diagnostic push") \
+        _Pragma("GCC diagnostic ignored \"-Woverride-init\"") \
+        int8_t __rc = network_dns_query_result_get_soa_internal(r,(network_dns_query_soa_result_t){ .index = 0, .mname = NULL, .serial = NULL, .refresh = NULL, .retry = NULL, .expire = NULL, .minimum = NULL, .rname = (rn) ,##__VA_ARGS__}); \
+        _Pragma("GCC diagnostic pop") \
+        __rc; \
+        })
 
 #ifdef __cplusplus
 }
